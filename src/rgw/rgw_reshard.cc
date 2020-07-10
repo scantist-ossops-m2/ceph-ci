91c776720253869998d2bd3d4999858ae5f73ece
Shilpa Jagannath <smanjara@redhat.com>
2020-07-10 13:35:25 +0530

    rgw: - take non-optional gen_id in svc_bi_rados functions
         - call init_index() on target layout during reshard process.
           Takes const rgw::bucket_index_layout_generation&
    
    Signed-off-by: Shilpa Jagannath <smanjara@redhat.com>
    (cherry picked from commit 3d8dfb752e3483aad9c232c9424a2090de2a8f6f)
    
    Conflicts:
    	src/rgw/rgw_bucket.cc
    	src/rgw/rgw_rados.cc
    	src/rgw/rgw_reshard.cc
    	src/rgw/services/svc_bi.h
    	src/rgw/services/svc_bi_rados.cc
    	src/rgw/services/svc_bi_rados.h
     - Upstream dpp
    
    Resolves: rhbz#1494059
    Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

--- src/rgw/rgw_reshard.cc
+++ src/rgw/rgw_reshard.cc
@@ -544,6 +544,7 @@ int RGWBucketReshard::do_reshard(int num_shards,
   }
 
   //increment generation number
+  bucket_info.layout.target_index->gen = bucket_info.layout.current_index.gen;
   bucket_info.layout.target_index->gen++;
 
   int num_target_shards = bucket_info.layout.target_index->layout.normal.num_shards;
@@ -573,7 +574,7 @@ int RGWBucketReshard::do_reshard(int num_shards,
     while (is_truncated) {
       entries.clear();
       ret = store->getRados()->bi_list(dpp, bucket_info, i, null_object_filter, marker, max_entries, &entries, &is_truncated);
-      if (ret < 0 && ret == -ENOENT) {
+      if (ret < 0 && ret != -ENOENT) {
         derr << "ERROR: bi_list(): " << cpp_strerror(-ret) << dendl;
         return ret;
       }
@@ -649,7 +650,7 @@ int RGWBucketReshard::do_reshard(int num_shards,
   } else if (out) {
     (*out) << " " << total_entries << std::endl;
   }
-
+  
   ret = target_shards_mgr.finish();
   if (ret < 0) {
     ldpp_dout(dpp, -1) << "ERROR: failed to reshard" << dendl;
@@ -664,6 +665,11 @@ int RGWBucketReshard::do_reshard(int num_shards,
 						    real_time(), &bucket_attrs, dpp);
   if (ret < 0) {
     ldpp_dout(dpp, -1) << "ERROR: failed writing bucket instance info: " << dendl;
+      return ret;
+  }
+
+  ret = store->svc()->bi->init_index(dpp, bucket_info, bucket_info.layout.current_index);
+  if (ret < 0) {
       return ret;
   }
 
@@ -717,7 +723,7 @@ int RGWBucketReshard::execute(int num_shards, int max_op_entries,
    // at this point since all we're using a best effor to to remove old
    // shard objects
 
-  ret = store->svc()->bi->clean_index(dpp, bucket_info, std::nullopt);
+  ret = store->svc()->bi->clean_index(dpp, bucket_info, bucket_info.layout.current_index.gen);
    if (ret < 0) {
      ldpp_dout(dpp, -1) << "Error: " << __func__ <<
       " failed to clean up old shards; " <<
