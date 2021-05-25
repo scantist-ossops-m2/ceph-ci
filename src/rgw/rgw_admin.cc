2dbe0151c9346e03b8c9454be3a802edd2bffbe1
Yuval Lifshitz <ylifshit@redhat.com>
2021-05-25 21:11:25 +0300

    rgw/multisite: track shard sync status objects per generation
    Signed-off-by: Yuval Lifshitz <ylifshit@redhat.com>
    (cherry picked from commit efc03b03256092e351ee24e779006144f26e8311)
    
    Conflicts:
    	src/rgw/rgw_admin.cc
    	src/rgw/rgw_rest_log.cc
     - Upstream dpp
     - Upstream zipper
    
    Resolves: rhbz#1494059
    Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -2321,7 +2321,7 @@ static int bucket_source_sync_status(const DoutPrefixProvider *dpp, rgw::sal::RG
   }
 
   std::vector<rgw_bucket_shard_sync_info> status;
-  r = rgw_read_bucket_inc_sync_status(dpp, store, pipe, bucket_info, &source_bucket_info, &status);
+  r = rgw_read_bucket_inc_sync_status(dpp, store, pipe, bucket_info, &source_bucket_info, full_status.incremental_gen,  &status);
   if (r < 0) {
     ldpp_dout(dpp, -1) << "failed to read bucket incremental sync status: " << cpp_strerror(r) << dendl;
     return r;
