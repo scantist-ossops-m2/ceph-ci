2f8d2aba6a11683f4317fbb5d7b236299befff80
J. Eric Ivancich <ivancich@redhat.com>
2021-07-28 13:52:29 -0400

    rgw: user stats showing 0 value for "size_utilized" and "size_kb_utilized" fields
    When accumulating user stats, the "utilized" fields are not looked
    at. Updates RGWStorageStats::dump so it only outputs the "utilized"
    data if they're updated.
    
    Signed-off-by: J. Eric Ivancich <ivancich@redhat.com>
    (cherry picked from commit 248fbce6b54f6c91e63b05861f8631ca64c8df81)
    
    Resolves: rhbz#1986160

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -7541,7 +7541,8 @@ next:
       }
     }
 
-    RGWStorageStats stats;
+    constexpr bool omit_utilized_stats = false;
+    RGWStorageStats stats(omit_utilized_stats);
     ceph::real_time last_stats_sync;
     ceph::real_time last_stats_update;
     int ret = store->ctl()->user->read_stats(dpp(), user_id, &stats, null_yield,
