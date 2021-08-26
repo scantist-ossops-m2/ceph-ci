fc815aa476f5dc35c3f438a37d2d8886c415bf59
Marcus Watts <mwatts@redhat.com>
2021-08-26 05:13:36 -0400

    rgw: defensive etag handling fixes
    Fix etags fetched from the wire or disk.  This change is for omap entries,
    mostly fetched via cls logic from the osds.
    
    Fixes: https://tracker.ceph.com/issues/?????
    
    Signed-off-by: Marcus Watts <mwatts@redhat.com>
    Resolves: rhbz#2011456

--- src/rgw/rgw_rados.cc
+++ src/rgw/rgw_rados.cc
@@ -1887,6 +1887,8 @@ int RGWRados::Bucket::List::list_objects_ordered(
       rgw_obj_index_key index_key = entry.key;
       rgw_obj_key obj(index_key);
 
+      rgw_fix_etag(cct, entry.meta.etag);
+
       ldpp_dout(dpp, 20) << "RGWRados::Bucket::List::" << __func__ <<
 	" considering entry " << entry.key << dendl;
 
@@ -8080,6 +8082,7 @@ int RGWRados::bi_get_instance(const DoutPrefixProvider *dpp, const RGWBucketInfo
     ldpp_dout(dpp, 0) << "ERROR: failed to decode bi_entry()" << dendl;
     return -EIO;
   }
+  rgw_fix_etag(cct, dirent->meta.etag);
 
   return 0;
 }
@@ -8782,6 +8785,9 @@ int RGWRados::cls_bucket_list_unordered(const DoutPrefixProvider *dpp,
 
       bool force_check = force_check_filter &&
 	force_check_filter(dirent.key.name);
+
+      rgw_fix_etag(cct, dirent.meta.etag);
+
       if ((!dirent.exists && !dirent.is_delete_marker()) ||
 	  !dirent.pending_map.empty() ||
 	  force_check) {
