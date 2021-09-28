fa2612abe5fd28987ac355c2529c7aaab963b733
Casey Bodley <cbodley@redhat.com>
2021-09-28 10:45:27 -0400

    rgw: prevent reshard from creating too many log generations
    Signed-off-by: Casey Bodley <cbodley@redhat.com>
    (cherry picked from commit c04590b7647d27bdb51d404269737c040fef5357)
    
    Conflicts:
    	src/rgw/rgw_admin.cc
     - Upstream zipper
    
    Resolves: rhbz#1494059
    Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -7097,6 +7097,17 @@ next:
       return ret;
     }
 
+    auto zone_svc = store->svc()->zone;
+    if (!RGWBucketReshard::can_reshard(bucket_info, zone_svc) &&
+        !yes_i_really_mean_it) {
+      std::cerr << "Bucket '" << bucket.name << "' already has too many "
+          "log generations (" << bucket_info.layout.logs.size() << ") "
+          "from previous reshards that peer zones haven't finished syncing. "
+          "Resharding is not recommended until the old generations sync, but "
+          "you can force a reshard with --yes-i-really-mean-it." << std::endl;
+      return -EINVAL;
+    }
+
     RGWBucketReshard br(store, bucket_info, nullptr /* no callback */);
 
 #define DEFAULT_RESHARD_MAX_ENTRIES 1000
