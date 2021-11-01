0c1b902db2bf9ebbb356f2820c915581ae2934fa
Casey Bodley <cbodley@redhat.com>
2021-11-01 13:14:16 -0400

    spawn: use explicit strand executor
    the default spawn::yield_context uses the polymorphic boost::asio::executor
    to support any executor type
    
    rgw's beast frontend always uses the same executor type for these
    coroutines, so we can use that type directly to avoid the overhead of
    type erasure and virtual function calls
    
    Resolves: rhbz#1964143
    
    Signed-off-by: Casey Bodley <cbodley@redhat.com>
    (cherry picked from commit 9d9258e06b78bb47fd0156d9bd7bb00b52a726b0)

--- src/rgw/rgw_sync_checkpoint.cc
+++ src/rgw/rgw_sync_checkpoint.cc
@@ -225,7 +225,7 @@ int rgw_bucket_sync_checkpoint(const DoutPrefixProvider* dpp,
     entry.pipe = pipe;
 
     // fetch remote markers
-    spawn::spawn(ioctx, [&] (spawn::yield_context yield) {
+    spawn::spawn(ioctx, [&] (yield_context yield) {
       auto y = optional_yield{ioctx, yield};
       rgw_bucket_index_marker_info info;
       int r = source_bilog_info(dpp, store->svc()->zone, entry.pipe,
@@ -238,7 +238,7 @@ int rgw_bucket_sync_checkpoint(const DoutPrefixProvider* dpp,
       entry.latest_gen = info.latest_gen;
     });
     // fetch source bucket info
-    spawn::spawn(ioctx, [&] (spawn::yield_context yield) {
+    spawn::spawn(ioctx, [&] (yield_context yield) {
       auto y = optional_yield{ioctx, yield};
       auto obj_ctx = store->svc()->sysobj->init_obj_ctx();
       int r = store->getRados()->get_bucket_instance_info(
