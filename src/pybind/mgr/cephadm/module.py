830070a3673ce38f7447fdcd0cde56a30a4f1192
Sage Weil <sage@newdream.net>
2021-11-01 18:03:00 -0400

    mgr/cephadm: add rotate support for rgw, crash, nfs, rbd-mirror, iscsi
    Signed-off-by: Sage Weil <sage@newdream.net>

--- src/pybind/mgr/cephadm/module.py
+++ src/pybind/mgr/cephadm/module.py
@@ -1817,7 +1817,7 @@ Then run the following:
         self.log.info(f'Rotating authentication key for {daemon_spec.name()}')
         rc, out, err = self.mon_command({
             'prefix': 'auth get-or-create-pending',
-            'entity': daemon_spec.name(),
+            'entity': daemon_spec.entity_name(),
             'format': 'json',
         })
         j = json.loads(out)
@@ -1927,7 +1927,8 @@ Then run the following:
                 f'Unable to schedule redeploy for {daemon_name}: No standby MGRs')
 
         if action == 'rotate-key':
-            if d.daemon_type not in ['mgr', 'osd', 'mds']:
+            if d.daemon_type not in ['mgr', 'osd', 'mds',
+                                     'rgw', 'crash', 'nfs', 'rbd-mirror', 'iscsi']:
                 raise OrchestratorError(
                     f'key rotation not supported for {d.daemon_type}'
                 )
