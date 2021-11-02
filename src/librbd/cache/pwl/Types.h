CongMinYin/ceph

librbd/cache/pwl/ssd: change disk format
remove cur_sync_gen because cur_sync_gen doesn't appear to be used anywhere.
remove #ifdef, use union include rwl and ssd field.

Signed-off-by: Yin Congmin <congmin.yin@intel.com>

src/librbd/cache/pwl/Types.h
@@ -175,7 +175,7 @@ const uint64_t DEFAULT_POOL_SIZE = 1u<<30;
 const uint64_t MIN_POOL_SIZE = DEFAULT_POOL_SIZE;
 constexpr double USABLE_SIZE = (7.0 / 10);
 const uint64_t BLOCK_ALLOC_OVERHEAD_BYTES = 16;
-const uint8_t RWL_POOL_VERSION = 1;
+const uint8_t RWL_POOL_VERSION = 2;
 const uint64_t SSD_POOL_VERSION = 1;
 const uint64_t MAX_LOG_ENTRIES = (1024 * 1024);
 const double AGGRESSIVE_RETIRE_HIGH_WATER = 0.75;
@@ -212,27 +212,34 @@ struct WriteLogCacheEntry {
   uint64_t write_sequence_number = 0;
   uint64_t image_offset_bytes;
   uint64_t write_bytes;
-  #ifdef WITH_RBD_RWL
-  TOID(uint8_t) write_data;
-  #endif
-  #ifdef WITH_RBD_SSD_CACHE
-  uint64_t write_data_pos = 0; /* SSD data offset */
-  #endif
   union {
-    uint8_t flags;
+    uint64_t flags;
     struct {
-      uint8_t entry_valid :1; /* if 0, this entry is free */
-      uint8_t sync_point :1;  /* No data. No write sequence number. Marks sync
+      uint64_t entry_valid :1; /* if 0, this entry is free */
+      uint64_t sync_point :1;  /* No data. No write sequence number. Marks sync
                                  point for this sync gen number */
-      uint8_t sequenced :1;   /* write sequence number is valid */
-      uint8_t has_data :1;    /* write_data field is valid (else ignore) */
-      uint8_t discard :1;     /* has_data will be 0 if this is a discard */
-      uint8_t writesame :1;   /* ws_datalen indicates length of data at write_bytes */
+      uint64_t sequenced :1;   /* write sequence number is valid */
+      uint64_t has_data :1;    /* write_data field is valid (else ignore) */
+      uint64_t discard :1;     /* has_data will be 0 if this is a discard */
+      uint64_t writesame :1;   /* ws_datalen indicates length of data at write_bytes */
     };
   };
-  uint32_t ws_datalen = 0;  /* Length of data buffer (writesame only) */
-  uint32_t entry_index = 0; /* For debug consistency check. Can be removed if
+  uint64_t ws_datalen = 0;  /* Length of data buffer (writesame only) */
+  uint64_t entry_index = 0; /* For debug consistency check. Can be removed if
                              * we need the space */
+  union {
+    /* RWL */
+    struct {
+      #ifdef WITH_RBD_RWL
+      PMEMoid write_data;
+      #endif
+    };
+    /* SSD */
+    struct {
+      uint64_t write_data_pos = 0; /* data offset */
+    };
+  };
+
   WriteLogCacheEntry(uint64_t image_offset_bytes=0, uint64_t write_bytes=0)
     : image_offset_bytes(image_offset_bytes), write_bytes(write_bytes),
       entry_valid(0), sync_point(0), sequenced(0), has_data(0), discard(0), writesame(0) {
@@ -278,32 +285,32 @@ struct WriteLogCacheEntry {
 };
 
 struct WriteLogPoolRoot {
-  #ifdef WITH_RBD_RWL
-  union {
-    struct {
-      uint8_t layout_version;    /* Version of this structure (RWL_POOL_VERSION) */
-    };
-    uint64_t _u64;
-  } header;
-  TOID(struct WriteLogCacheEntry) log_entries;   /* contiguous array of log entries */
-  #endif
-  #ifdef WITH_RBD_SSD_CACHE
-  uint64_t layout_version = 0;
-  uint64_t cur_sync_gen = 0;
-  #endif
+  uint64_t layout_version;
   uint64_t pool_size;
   uint64_t flushed_sync_gen;     /* All writing entries with this or a lower
                                   * sync gen number are flushed. */
   uint32_t block_size;           /* block size */
-  uint32_t num_log_entries;
+  uint64_t num_log_entries;
   uint64_t first_free_entry;     /* Entry following the newest valid entry */
   uint64_t first_valid_entry;    /* Index of the oldest valid entry in the log */
+
+  union {
+    /* RWL */
+    struct {
+      #ifdef WITH_RBD_RWL
+      PMEMoid log_entries;      /* contiguous array of log entries */
+      #endif
+    };
+    /* SSD */
+    struct {
+      uint64_t unused;
+    };
+  };
 
   #ifdef WITH_RBD_SSD_CACHE
   DENC(WriteLogPoolRoot, v, p) {
     DENC_START(1, 1, p);
     denc(v.layout_version, p);
-    denc(v.cur_sync_gen, p);
     denc(v.pool_size, p);
     denc(v.flushed_sync_gen, p);
     denc(v.block_size, p);

src/librbd/cache/pwl/rwl/WriteLog.cc
@@ -98,7 +98,8 @@ void WriteLog<I>::alloc_op_log_entries(GenericLogOperations &ops)
 {
   TOID(struct WriteLogPoolRoot) pool_root;
   pool_root = POBJ_ROOT(m_log_pool, struct WriteLogPoolRoot);
-  struct WriteLogCacheEntry *pmem_log_entries = D_RW(D_RW(pool_root)->log_entries);
+  TOID(struct WriteLogCacheEntry) _log_entries = D_RW(pool_root)->log_entries;
+  struct WriteLogCacheEntry *pmem_log_entries = D_RW(_log_entries);
 
   ceph_assert(ceph_mutex_is_locked_by_me(this->m_log_append_lock));
 
@@ -300,10 +301,11 @@ bool WriteLog<I>::initialize_pool(Context *on_finish, pwl::DeferredContexts &lat
     m_first_valid_entry = 0;
     TX_BEGIN(m_log_pool) {
       TX_ADD(pool_root);
-      D_RW(pool_root)->header.layout_version = RWL_POOL_VERSION;
-      D_RW(pool_root)->log_entries =
-        TX_ZALLOC(struct WriteLogCacheEntry,
-                  sizeof(struct WriteLogCacheEntry) * num_small_writes);
+      D_RW(pool_root)->layout_version = RWL_POOL_VERSION;
+      TOID(struct WriteLogCacheEntry) _log_entries;
+      _log_entries = TX_ZALLOC(struct WriteLogCacheEntry,
+                     sizeof(struct WriteLogCacheEntry) * num_small_writes);
+      D_RW(pool_root)->log_entries = _log_entries.oid;
       D_RW(pool_root)->pool_size = this->m_log_pool_size;
       D_RW(pool_root)->flushed_sync_gen = this->m_flushed_sync_gen;
       D_RW(pool_root)->block_size = MIN_WRITE_ALLOC_SIZE;
@@ -333,10 +335,10 @@ bool WriteLog<I>::initialize_pool(Context *on_finish, pwl::DeferredContexts &lat
       return false;
     }
     pool_root = POBJ_ROOT(m_log_pool, struct WriteLogPoolRoot);
-    if (D_RO(pool_root)->header.layout_version != RWL_POOL_VERSION) {
+    if (D_RO(pool_root)->layout_version != RWL_POOL_VERSION) {
       // TODO: will handle upgrading version in the future
       lderr(cct) << "Pool layout version is "
-                 << D_RO(pool_root)->header.layout_version
+                 << D_RO(pool_root)->layout_version
                  << " expected " << RWL_POOL_VERSION << dendl;
       on_finish->complete(-EINVAL);
       return false;
@@ -395,7 +397,8 @@ template <typename I>
 void WriteLog<I>::load_existing_entries(DeferredContexts &later) {
   TOID(struct WriteLogPoolRoot) pool_root;
   pool_root = POBJ_ROOT(m_log_pool, struct WriteLogPoolRoot);
-  struct WriteLogCacheEntry *pmem_log_entries = D_RW(D_RW(pool_root)->log_entries);
+  TOID(struct WriteLogCacheEntry) _log_entries= D_RW(pool_root)->log_entries;
+  struct WriteLogCacheEntry *pmem_log_entries = D_RW(_log_entries);
   uint64_t entry_index = m_first_valid_entry;
   /* The map below allows us to find sync point log entries by sync
    * gen number, which is necessary so write entries can be linked to
@@ -442,7 +445,8 @@ template <typename I>
 void WriteLog<I>::write_data_to_buffer(
     std::shared_ptr<pwl::WriteLogEntry> ws_entry,
     WriteLogCacheEntry *pmem_entry) {
-  ws_entry->cache_buffer = D_RW(pmem_entry->write_data);
+  TOID(uint8_t) _write_data = pmem_entry->write_data;
+  ws_entry->cache_buffer = D_RW(_write_data);
 }
 
 /**
@@ -514,11 +518,13 @@ bool WriteLog<I>::retire_entries(const unsigned long int frees_per_tx) {
           D_RW(pool_root)->flushed_sync_gen = flushed_sync_gen;
         }
         D_RW(pool_root)->first_valid_entry = first_valid_entry;
+        TOID(uint8_t) _write_data;
         for (auto &entry: retiring_entries) {
           if (entry->write_bytes()) {
-            ldout(cct, 20) << "Freeing " << entry->ram_entry.write_data.oid.pool_uuid_lo
-                           << "." << entry->ram_entry.write_data.oid.off << dendl;
-            TX_FREE(entry->ram_entry.write_data);
+            ldout(cct, 20) << "Freeing " << entry->ram_entry.write_data.pool_uuid_lo
+                           << "." << entry->ram_entry.write_data.off << dendl;
+            _write_data = entry->ram_entry.write_data;
+            TX_FREE(_write_data);
           } else {
             ldout(cct, 20) << "Retiring non-write: " << *entry << dendl;
           }

scantist-ossops-m2/ceph-ci

src/librbd/cache/pwl/Types.h
@@ -177,8 +177,8 @@ const uint64_t MIN_POOL_SIZE = DEFAULT_POOL_SIZE;
 const uint64_t POOL_SIZE_ALIGN = 1 << 20;
 constexpr double USABLE_SIZE = (7.0 / 10);
 const uint64_t BLOCK_ALLOC_OVERHEAD_BYTES = 16;
-const uint8_t RWL_POOL_VERSION = 1;
-const uint64_t SSD_POOL_VERSION = 1;
+const uint8_t RWL_LAYOUT_VERSION = 2;
+const uint8_t SSD_LAYOUT_VERSION = 1;
 const uint64_t MAX_LOG_ENTRIES = (1024 * 1024);
 const double AGGRESSIVE_RETIRE_HIGH_WATER = 0.75;
 const double RETIRE_HIGH_WATER = 0.50;
@@ -214,27 +214,32 @@ struct WriteLogCacheEntry {
   uint64_t write_sequence_number = 0;
   uint64_t image_offset_bytes;
   uint64_t write_bytes;
-  #ifdef WITH_RBD_RWL
-  TOID(uint8_t) write_data;
-  #endif
-  #ifdef WITH_RBD_SSD_CACHE
-  uint64_t write_data_pos = 0; /* SSD data offset */
-  #endif
   union {
-    uint8_t flags;
+    uint64_t flags;
     struct {
-      uint8_t entry_valid :1; /* if 0, this entry is free */
-      uint8_t sync_point :1;  /* No data. No write sequence number. Marks sync
+      uint64_t entry_valid :1; /* if 0, this entry is free */
+      uint64_t sync_point :1;  /* No data. No write sequence number. Marks sync
                                  point for this sync gen number */
-      uint8_t sequenced :1;   /* write sequence number is valid */
-      uint8_t has_data :1;    /* write_data field is valid (else ignore) */
-      uint8_t discard :1;     /* has_data will be 0 if this is a discard */
-      uint8_t writesame :1;   /* ws_datalen indicates length of data at write_bytes */
+      uint64_t sequenced :1;   /* write sequence number is valid */
+      uint64_t has_data :1;    /* write_data field is valid (else ignore) */
+      uint64_t discard :1;     /* has_data will be 0 if this is a discard */
+      uint64_t writesame :1;   /* ws_datalen indicates length of data at write_bytes */
     };
   };
-  uint32_t ws_datalen = 0;  /* Length of data buffer (writesame only) */
-  uint32_t entry_index = 0; /* For debug consistency check. Can be removed if
-                             * we need the space */
+  uint64_t ws_datalen = 0;     /* Length of data buffer (writesame only) */
+  union {
+    /* RWL */
+    struct {
+      #ifdef WITH_RBD_RWL
+      PMEMoid write_data;
+      #endif
+    };
+    /* SSD */
+    struct {
+      uint64_t write_data_pos = 0; /* data offset */
+    };
+  };
+
   WriteLogCacheEntry(uint64_t image_offset_bytes=0, uint64_t write_bytes=0)
     : image_offset_bytes(image_offset_bytes), write_bytes(write_bytes),
       entry_valid(0), sync_point(0), sequenced(0), has_data(0), discard(0), writesame(0) {
@@ -271,7 +276,6 @@ struct WriteLogCacheEntry {
     denc(v.write_data_pos, p);
     denc(v.flags, p);
     denc(v.ws_datalen, p);
-    denc(v.entry_index, p);
     DENC_FINISH(p);
   }
   #endif
@@ -280,32 +284,32 @@ struct WriteLogCacheEntry {
 };
 
 struct WriteLogPoolRoot {
-  #ifdef WITH_RBD_RWL
-  union {
-    struct {
-      uint8_t layout_version;    /* Version of this structure (RWL_POOL_VERSION) */
-    };
-    uint64_t _u64;
-  } header;
-  TOID(struct WriteLogCacheEntry) log_entries;   /* contiguous array of log entries */
-  #endif
-  #ifdef WITH_RBD_SSD_CACHE
-  uint64_t layout_version = 0;
-  uint64_t cur_sync_gen = 0;
-  #endif
+  uint64_t layout_version;
   uint64_t pool_size;
   uint64_t flushed_sync_gen;     /* All writing entries with this or a lower
                                   * sync gen number are flushed. */
-  uint32_t block_size;           /* block size */
-  uint32_t num_log_entries;
+  uint64_t block_size;           /* block size */
+  uint64_t num_log_entries;
   uint64_t first_free_entry;     /* Entry following the newest valid entry */
   uint64_t first_valid_entry;    /* Index of the oldest valid entry in the log */
+
+  union {
+    /* RWL */
+    struct {
+      #ifdef WITH_RBD_RWL
+      PMEMoid log_entries;      /* contiguous array of log entries */
+      #endif
+    };
+    /* SSD */
+    struct {
+      uint64_t unused;
+    };
+  };
 
   #ifdef WITH_RBD_SSD_CACHE
   DENC(WriteLogPoolRoot, v, p) {
     DENC_START(1, 1, p);
     denc(v.layout_version, p);
-    denc(v.cur_sync_gen, p);
     denc(v.pool_size, p);
     denc(v.flushed_sync_gen, p);
     denc(v.block_size, p);

src/librbd/cache/pwl/rwl/WriteLog.cc
@@ -99,7 +99,8 @@ void WriteLog<I>::alloc_op_log_entries(GenericLogOperations &ops)
 {
   TOID(struct WriteLogPoolRoot) pool_root;
   pool_root = POBJ_ROOT(m_log_pool, struct WriteLogPoolRoot);
-  struct WriteLogCacheEntry *pmem_log_entries = D_RW(D_RW(pool_root)->log_entries);
+  TOID(struct WriteLogCacheEntry) _log_entries = D_RW(pool_root)->log_entries;
+  struct WriteLogCacheEntry *pmem_log_entries = D_RW(_log_entries);
 
   ceph_assert(ceph_mutex_is_locked_by_me(this->m_log_append_lock));
 
@@ -111,7 +112,6 @@ void WriteLog<I>::alloc_op_log_entries(GenericLogOperations &ops)
     this->m_first_free_entry = (this->m_first_free_entry + 1) % this->m_total_log_entries;
     auto &log_entry = operation->get_log_entry();
     log_entry->log_entry_index = entry_index;
-    log_entry->ram_entry.entry_index = entry_index;
     log_entry->cache_entry = &pmem_log_entries[entry_index];
     log_entry->ram_entry.entry_valid = 1;
     m_log_entries.push_back(log_entry);
@@ -301,10 +301,11 @@ bool WriteLog<I>::initialize_pool(Context *on_finish, pwl::DeferredContexts &lat
     m_first_valid_entry = 0;
     TX_BEGIN(m_log_pool) {
       TX_ADD(pool_root);
-      D_RW(pool_root)->header.layout_version = RWL_POOL_VERSION;
-      D_RW(pool_root)->log_entries =
-        TX_ZALLOC(struct WriteLogCacheEntry,
-                  sizeof(struct WriteLogCacheEntry) * num_small_writes);
+      D_RW(pool_root)->layout_version = RWL_LAYOUT_VERSION;
+      TOID(struct WriteLogCacheEntry) _log_entries;
+      _log_entries = TX_ZALLOC(struct WriteLogCacheEntry,
+                     sizeof(struct WriteLogCacheEntry) * num_small_writes);
+      D_RW(pool_root)->log_entries = _log_entries.oid;
       D_RW(pool_root)->pool_size = this->m_log_pool_size;
       D_RW(pool_root)->flushed_sync_gen = this->m_flushed_sync_gen;
       D_RW(pool_root)->block_size = MIN_WRITE_ALLOC_SIZE;
@@ -334,11 +335,11 @@ bool WriteLog<I>::initialize_pool(Context *on_finish, pwl::DeferredContexts &lat
       return false;
     }
     pool_root = POBJ_ROOT(m_log_pool, struct WriteLogPoolRoot);
-    if (D_RO(pool_root)->header.layout_version != RWL_POOL_VERSION) {
+    if (D_RO(pool_root)->layout_version != RWL_LAYOUT_VERSION) {
       // TODO: will handle upgrading version in the future
       lderr(cct) << "Pool layout version is "
-                 << D_RO(pool_root)->header.layout_version
-                 << " expected " << RWL_POOL_VERSION << dendl;
+                 << D_RO(pool_root)->layout_version
+                 << " expected " << RWL_LAYOUT_VERSION << dendl;
       on_finish->complete(-EINVAL);
       return false;
     }
@@ -396,7 +397,8 @@ template <typename I>
 void WriteLog<I>::load_existing_entries(DeferredContexts &later) {
   TOID(struct WriteLogPoolRoot) pool_root;
   pool_root = POBJ_ROOT(m_log_pool, struct WriteLogPoolRoot);
-  struct WriteLogCacheEntry *pmem_log_entries = D_RW(D_RW(pool_root)->log_entries);
+  TOID(struct WriteLogCacheEntry) _log_entries= D_RW(pool_root)->log_entries;
+  struct WriteLogCacheEntry *pmem_log_entries = D_RW(_log_entries);
   uint64_t entry_index = m_first_valid_entry;
   /* The map below allows us to find sync point log entries by sync
    * gen number, which is necessary so write entries can be linked to
@@ -421,7 +423,6 @@ void WriteLog<I>::load_existing_entries(DeferredContexts &later) {
   while (entry_index != m_first_free_entry) {
     WriteLogCacheEntry *pmem_entry = &pmem_log_entries[entry_index];
     std::shared_ptr<GenericLogEntry> log_entry = nullptr;
-    ceph_assert(pmem_entry->entry_index == entry_index);
 
     this->update_entries(&log_entry, pmem_entry, missing_sync_points,
         sync_point_entries, entry_index);
@@ -452,7 +453,8 @@ template <typename I>
 void WriteLog<I>::write_data_to_buffer(
     std::shared_ptr<pwl::WriteLogEntry> ws_entry,
     WriteLogCacheEntry *pmem_entry) {
-  ws_entry->cache_buffer = D_RW(pmem_entry->write_data);
+  TOID(uint8_t) _write_data = pmem_entry->write_data;
+  ws_entry->cache_buffer = D_RW(_write_data);
 }
 
 /**
@@ -524,11 +526,13 @@ bool WriteLog<I>::retire_entries(const unsigned long int frees_per_tx) {
           D_RW(pool_root)->flushed_sync_gen = flushed_sync_gen;
         }
         D_RW(pool_root)->first_valid_entry = first_valid_entry;
+        TOID(uint8_t) _write_data;
         for (auto &entry: retiring_entries) {
           if (entry->write_bytes()) {
-            ldout(cct, 20) << "Freeing " << entry->ram_entry.write_data.oid.pool_uuid_lo
-                           << "." << entry->ram_entry.write_data.oid.off << dendl;
-            TX_FREE(entry->ram_entry.write_data);
+            ldout(cct, 20) << "Freeing " << entry->ram_entry.write_data.pool_uuid_lo
+                           << "." << entry->ram_entry.write_data.off << dendl;
+            _write_data = entry->ram_entry.write_data;
+            TX_FREE(_write_data);
           } else {
             ldout(cct, 20) << "Retiring non-write: " << *entry << dendl;
           }