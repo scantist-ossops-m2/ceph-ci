18832c69854afd85e39f322a68ae726544949e50
Nizamudeen A <nia@redhat.com>
2021-11-16 16:30:02 +0530

    mgr/dashboard: Implement drain host functionality in dashboard
    Resolves: rhbz#1982965
    
    Fixes: https://tracker.ceph.com/issues/51587
    Signed-off-by: Nizamudeen A <nia@redhat.com>
    (cherry picked from commit 524c340562d6b44c329ffa635cf3203e059171ad)
    (cherry picked from commit aa6eb38015ac5a8473da24efad78520a4c790c95)

--- src/pybind/mgr/dashboard/frontend/src/app/shared/constants/app.constants.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/constants/app.constants.ts
@@ -121,6 +121,8 @@ export class ActionLabelsI18n {
   FLAGS: string;
   ENTER_MAINTENANCE: string;
   EXIT_MAINTENANCE: string;
+  START_DRAIN: string;
+  STOP_DRAIN: string;
 
   constructor() {
     /* Create a new item */
@@ -174,6 +176,8 @@ export class ActionLabelsI18n {
     this.FLAGS = $localize`Flags`;
     this.ENTER_MAINTENANCE = $localize`Enter Maintenance`;
     this.EXIT_MAINTENANCE = $localize`Exit Maintenance`;
+    this.START_DRAIN = $localize`Start Drain`;
+    this.STOP_DRAIN = $localize`Stop Drain`;
 
     /* Prometheus wording */
     this.RECREATE = $localize`Recreate`;
