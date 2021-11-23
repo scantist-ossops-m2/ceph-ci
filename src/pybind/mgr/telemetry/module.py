bb02fc6245a844ef9e743b97ab424b2ea51a04d0
Yaarit Hatuka <yaarit@redhat.com>
2021-11-23 00:12:10 +0000

    mgr/telemetry: add commands to enable/disable channels
    Currently we enable/disable a telemetry channel via CLI with:
      `ceph config set mgr mgr/telemetry/channel_basic true`
      `ceph config set mgr mgr/telemetry/channel_crash false`
    
    We can now do this with:
      `ceph telemetry enable channel basic`
      `ceph telemetry disable channel crash`
    
    We allow enabling / disabling lists of channels:
      `ceph telemetry enable channel basic device crash perf`
      `ceph telemetry disable channel basic device crash perf`
    
    Please note, telemetry should be on for these commands to take effect.
    
    Signed-off-by: Yaarit Hatuka <yaarit@redhat.com>

--- src/pybind/mgr/telemetry/module.py
+++ src/pybind/mgr/telemetry/module.py
@@ -1289,6 +1289,34 @@ class Module(MgrModule):
             # histograms.
             pass
 
+    def toggle_channel(self, action, channels: Optional[List[str]] = None) -> Tuple[int, str, str]:
+        '''
+        Enable or disable a list of channels
+        '''
+        if not self.enabled:
+            # telemetry should be on for channels to be toggled
+            msg = 'Telemetry is off. Please consider opting-in with `ceph telemetry on`.\n' \
+                  'Preview sample reports with `ceph telemetry preview`.'
+            return 0, msg, ''
+
+        if channels is None:
+            msg = f'Please provide a channel name. Available channels: {ALL_CHANNELS}.'
+            return 0, msg, ''
+
+        state = action == 'enable'
+        msg = ''
+        for c in channels:
+            if c not in ALL_CHANNELS:
+                msg = f"{msg}{c} is not a valid channel name. "\
+                        f"Available channels: {ALL_CHANNELS}.\n"
+            else:
+                self.set_module_option(f"channel_{c}", state)
+                setattr(self,
+                        f"channel_{c}",
+                        state)
+                msg = f"{msg}channel_{c} is {action}d\n"
+
+        return 0, msg, ''
 
     @CLIReadCommand('telemetry status')
     def status(self) -> Tuple[int, str, str]:
@@ -1361,6 +1389,20 @@ To enable, add '--license {LICENSE}' to the 'ceph telemetry on' command.'''
         self.last_opted_out_ceph_version = mon_min
 
         return 0, '', ''
+
+    @CLIReadCommand('telemetry enable channel')
+    def enable_channel(self, channels: Optional[List[str]] = None) -> Tuple[int, str, str]:
+        '''
+        Enable a list of channels
+        '''
+        return self.toggle_channel('enable', channels)
+
+    @CLIReadCommand('telemetry disable channel')
+    def disable_channel(self, channels: Optional[List[str]] = None) -> Tuple[int, str, str]:
+        '''
+        Disable a list of channels
+        '''
+        return self.toggle_channel('disable', channels)
 
     @CLICommand('telemetry send')
     def do_send(self,
