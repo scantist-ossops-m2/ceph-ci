e2bb04d51b29e2d97ffb9c77394043fba2c228de
Marcus Watts <mwatts@redhat.com>
2021-12-17 23:16:09 -0500

    rgw/sse-s3: +get_encryption_defaults, use new crypt_attribute_map
    init_processing
    	new virtual method get_encryption_defaults at the end of this.
    	for s3 this will do more.
    verify_permisions
    	fetch encryption attributes from crypt_attribute_map not x_meta_map
    	for postobj, x_meta_map only gets meta attributes, not sse.
    	if bucketencryption policy exists, it *may* be correct to
    	prepopulate this before bucket policy sees it.
    
    Signed-off-by: Marcus Watts <mwatts@redhat.com>

--- src/rgw/rgw_op.h
+++ src/rgw/rgw_op.h
@@ -1264,6 +1264,10 @@ public:
   void pre_exec() override;
   void execute(optional_yield y) override;
 
+  virtual int get_encryption_defaults() {
+    return 0;
+  }
+
   /* this is for cases when copying data from other object */
   virtual int get_decrypt_filter(std::unique_ptr<RGWGetObj_Filter>* filter,
                                  RGWGetObj_Filter* cb,
