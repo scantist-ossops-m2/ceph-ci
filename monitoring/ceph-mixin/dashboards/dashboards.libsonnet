MrFreezeex/ceph

mgr/dashboard: monitoring: refactor into ceph-mixin
Mixin is a way to bundle dashboards, prometheus rules and alerts into
jsonnet package. Shifting to mixin will allow easier integration with
monitoring automation that some users may use.

This commit moves `/monitoring/grafana/dashboards` and
`/monitoring/prometheus` to `/monitoring/ceph-mixin`. Prometheus alerts
was also converted to Jsonnet using an automated way (from yaml to json
to jsonnet). This commit minimises any change made to the generated files
and should not change neithers the dashboards nor the Prometheus alerts.

In the future some configuration will also be added to jsonnet to add
more functionalities to the dashboards or alerts (i.e.: multi cluster).

Fixes: https://tracker.ceph.com/issues/53374
Signed-off-by: Arthur Outhenin-Chalandre <arthur.outhenin-chalandre@cern.ch>

monitoring/ceph-mixin/dashboards/dashboards.libsonnet
@@ -1,0 +1,4 @@
+(import 'general.libsonnet') +
+(import 'cephfs.libsonnet') +
+(import 'rbd.libsonnet') +
+(import 'rgw.libsonnet')

monitoring/ceph-mixin/dashboards/utils.libsonnet
@@ -1,0 +1,157 @@
+local g = import 'grafonnet/grafana.libsonnet';
+
+{
+  dashboardSchema(title,
+                  description,
+                  uid,
+                  time_from,
+                  refresh,
+                  schemaVersion,
+                  tags,
+                  timezone,
+                  timepicker)::
+    g.dashboard.new(title=title,
+                    description=description,
+                    uid=uid,
+                    time_from=time_from,
+                    refresh=refresh,
+                    schemaVersion=schemaVersion,
+                    tags=tags,
+                    timezone=timezone,
+                    timepicker=timepicker),
+
+  graphPanelSchema(aliasColors,
+                   title,
+                   description,
+                   nullPointMode,
+                   stack,
+                   formatY1,
+                   formatY2,
+                   labelY1,
+                   labelY2,
+                   min,
+                   fill,
+                   datasource)::
+    g.graphPanel.new(aliasColors=aliasColors,
+                     title=title,
+                     description=description,
+                     nullPointMode=nullPointMode,
+                     stack=stack,
+                     formatY1=formatY1,
+                     formatY2=formatY2,
+                     labelY1=labelY1,
+                     labelY2=labelY2,
+                     min=min,
+                     fill=fill,
+                     datasource=datasource),
+
+  addTargetSchema(expr, intervalFactor, format, legendFormat)::
+    g.prometheus.target(expr=expr,
+                        intervalFactor=intervalFactor,
+                        format=format,
+                        legendFormat=legendFormat),
+
+  addTemplateSchema(name,
+                    datasource,
+                    query,
+                    refresh,
+                    includeAll,
+                    sort,
+                    label,
+                    regex)::
+    g.template.new(name=name,
+                   datasource=datasource,
+                   query=query,
+                   refresh=refresh,
+                   includeAll=includeAll,
+                   sort=sort,
+                   label=label,
+                   regex=regex),
+
+  addAnnotationSchema(builtIn,
+                      datasource,
+                      enable,
+                      hide,
+                      iconColor,
+                      name,
+                      type)::
+    g.annotation.datasource(builtIn=builtIn,
+                            datasource=datasource,
+                            enable=enable,
+                            hide=hide,
+                            iconColor=iconColor,
+                            name=name,
+                            type=type),
+
+  addRowSchema(collapse, showTitle, title)::
+    g.row.new(collapse=collapse, showTitle=showTitle, title=title),
+
+  addSingleStatSchema(datasource,
+                      format,
+                      title,
+                      description,
+                      valueName,
+                      colorValue,
+                      gaugeMaxValue,
+                      gaugeShow,
+                      sparklineShow,
+                      thresholds)::
+    g.singlestat.new(datasource=datasource,
+                     format=format,
+                     title=title,
+                     description=description,
+                     valueName=valueName,
+                     colorValue=colorValue,
+                     gaugeMaxValue=gaugeMaxValue,
+                     gaugeShow=gaugeShow,
+                     sparklineShow=sparklineShow,
+                     thresholds=thresholds),
+
+  addPieChartSchema(aliasColors,
+                    datasource,
+                    description,
+                    legendType,
+                    pieType,
+                    title,
+                    valueName)::
+    g.pieChartPanel.new(aliasColors=aliasColors,
+                        datasource=datasource,
+                        description=description,
+                        legendType=legendType,
+                        pieType=pieType,
+                        title=title,
+                        valueName=valueName),
+
+  addTableSchema(datasource, description, sort, styles, title, transform)::
+    g.tablePanel.new(datasource=datasource,
+                     description=description,
+                     sort=sort,
+                     styles=styles,
+                     title=title,
+                     transform=transform),
+
+  addStyle(alias,
+           colorMode,
+           colors,
+           dateFormat,
+           decimals,
+           mappingType,
+           pattern,
+           thresholds,
+           type,
+           unit,
+           valueMaps)::
+    {
+      alias: alias,
+      colorMode: colorMode,
+      colors: colors,
+      dateFormat: dateFormat,
+      decimals: decimals,
+      mappingType: mappingType,
+      pattern: pattern,
+      thresholds: thresholds,
+      type: type,
+      unit: unit,
+      valueMaps: valueMaps,
+    },
+}

scantist-ossops-m2/ceph-ci

monitoring/ceph-mixin/dashboards/dashboards.libsonnet
@@ -1,0 +1,6 @@
+(import 'cephfs.libsonnet') +
+(import 'host.libsonnet') +
+(import 'osd.libsonnet') +
+(import 'pool.libsonnet') +
+(import 'rbd.libsonnet') +
+(import 'rgw.libsonnet')

monitoring/ceph-mixin/dashboards/utils.libsonnet
@@ -1,0 +1,172 @@
+local g = import 'grafonnet/grafana.libsonnet';
+
+{
+  dashboardSchema(title,
+                  description,
+                  uid,
+                  time_from,
+                  refresh,
+                  schemaVersion,
+                  tags,
+                  timezone,
+                  timepicker)::
+    g.dashboard.new(title=title,
+                    description=description,
+                    uid=uid,
+                    time_from=time_from,
+                    refresh=refresh,
+                    schemaVersion=schemaVersion,
+                    tags=tags,
+                    timezone=timezone,
+                    timepicker=timepicker),
+
+  graphPanelSchema(aliasColors,
+                   title,
+                   description,
+                   nullPointMode,
+                   stack,
+                   formatY1,
+                   formatY2,
+                   labelY1,
+                   labelY2,
+                   min,
+                   fill,
+                   datasource,
+                   legend_alignAsTable=false,
+                   legend_avg=false,
+                   legend_min=false,
+                   legend_max=false,
+                   legend_current=false,
+                   legend_values=false)::
+    g.graphPanel.new(aliasColors=aliasColors,
+                     title=title,
+                     description=description,
+                     nullPointMode=nullPointMode,
+                     stack=stack,
+                     formatY1=formatY1,
+                     formatY2=formatY2,
+                     labelY1=labelY1,
+                     labelY2=labelY2,
+                     min=min,
+                     fill=fill,
+                     datasource=datasource,
+                     legend_alignAsTable=legend_alignAsTable,
+                     legend_avg=legend_avg,
+                     legend_min=legend_min,
+                     legend_max=legend_max,
+                     legend_current=legend_current,
+                     legend_values=legend_values),
+
+
+  addTargetSchema(expr, intervalFactor, format, legendFormat)::
+    g.prometheus.target(expr=expr,
+                        intervalFactor=intervalFactor,
+                        format=format,
+                        legendFormat=legendFormat),
+
+  addTemplateSchema(name,
+                    datasource,
+                    query,
+                    refresh,
+                    includeAll,
+                    sort,
+                    label,
+                    regex)::
+    g.template.new(name=name,
+                   datasource=datasource,
+                   query=query,
+                   refresh=refresh,
+                   includeAll=includeAll,
+                   sort=sort,
+                   label=label,
+                   regex=regex),
+
+  addAnnotationSchema(builtIn,
+                      datasource,
+                      enable,
+                      hide,
+                      iconColor,
+                      name,
+                      type)::
+    g.annotation.datasource(builtIn=builtIn,
+                            datasource=datasource,
+                            enable=enable,
+                            hide=hide,
+                            iconColor=iconColor,
+                            name=name,
+                            type=type),
+
+  addRowSchema(collapse, showTitle, title)::
+    g.row.new(collapse=collapse, showTitle=showTitle, title=title),
+
+  addSingleStatSchema(colors,
+                      datasource,
+                      format,
+                      title,
+                      description,
+                      valueName,
+                      colorValue,
+                      gaugeMaxValue,
+                      gaugeShow,
+                      sparklineShow,
+                      thresholds)::
+    g.singlestat.new(colors=colors,
+                     datasource=datasource,
+                     format=format,
+                     title=title,
+                     description=description,
+                     valueName=valueName,
+                     colorValue=colorValue,
+                     gaugeMaxValue=gaugeMaxValue,
+                     gaugeShow=gaugeShow,
+                     sparklineShow=sparklineShow,
+                     thresholds=thresholds),
+
+  addPieChartSchema(aliasColors,
+                    datasource,
+                    description,
+                    legendType,
+                    pieType,
+                    title,
+                    valueName)::
+    g.pieChartPanel.new(aliasColors=aliasColors,
+                        datasource=datasource,
+                        description=description,
+                        legendType=legendType,
+                        pieType=pieType,
+                        title=title,
+                        valueName=valueName),
+
+  addTableSchema(datasource, description, sort, styles, title, transform)::
+    g.tablePanel.new(datasource=datasource,
+                     description=description,
+                     sort=sort,
+                     styles=styles,
+                     title=title,
+                     transform=transform),
+
+  addStyle(alias,
+           colorMode,
+           colors,
+           dateFormat,
+           decimals,
+           mappingType,
+           pattern,
+           thresholds,
+           type,
+           unit,
+           valueMaps)::
+    {
+      alias: alias,
+      colorMode: colorMode,
+      colors: colors,
+      dateFormat: dateFormat,
+      decimals: decimals,
+      mappingType: mappingType,
+      pattern: pattern,
+      thresholds: thresholds,
+      type: type,
+      unit: unit,
+      valueMaps: valueMaps,
+    },
+}