a311afd8512ef8c93df3d4603f58e816a404aa96
Omri Zeneva <ozeneva@redhat.com>
2022-01-20 04:03:53 -0500

    osd: remove unused tracing spans
    Signed-off-by: Omri Zeneva <ozeneva@redhat.com>

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -7264,7 +7264,6 @@ void OSD::dispatch_session_waiting(const ceph::ref_t<Session>& session, OSDMapRe
 
 void OSD::ms_fast_dispatch(Message *m)
 {
-  auto dispatch_span = tracing::osd::tracer.start_trace(__func__);
   FUNCTRACE(cct);
   if (service.is_stopping()) {
     m->put();
@@ -7320,7 +7319,7 @@ void OSD::ms_fast_dispatch(Message *m)
     tracepoint(osd, ms_fast_dispatch, reqid.name._type,
         reqid.name._num, reqid.tid, reqid.inc);
   }
-  op->osd_parent_span = tracing::osd::tracer.add_span("op-request-created", dispatch_span);
+  op->osd_parent_span = tracing::osd::tracer.start_trace("op-request-created");
 
   if (m->trace)
     op->osd_trace.init("osd op", &trace_endpoint, &m->trace);
