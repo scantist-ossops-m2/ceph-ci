cd366c2fda30fd7968121b2c5b349d646043d754
Matt Benjamin <mbenjamin@redhat.com>
2022-01-23 15:31:26 -0500

    rgw: introduce inventory configuration structures
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/rgw/rgw_inventory.h
+++ src/rgw/rgw_inventory.h
@@ -13,3 +13,83 @@
 #include "rgw_xml.h"
 
 
+namespace rgw { namespace inv {
+
+  enum class Format : uint8_t
+  {
+    CSV = 0,
+    ORC,
+    Parquet,
+  };
+
+  enum class Schedule : uint8_t
+  {
+    Daily = 0,
+    Weekly,
+  };
+
+  enum class ObjectVersions : uint8_t
+  {
+    All = 0,
+    Current,
+  };
+
+  enum class Field : uint8_t
+  {
+    Size = 0,
+    LastModifiedDate,
+    StorageClass,
+    ETag,
+    IsMultipartUploaded,
+    ReplicationStatus,
+    EncryptionStatus,
+    ObjectLockRetainUntilDate,
+    ObjectLockMode,
+    ObjectLockLegalHoldStatus,
+    IntelligentTieringAccessTier,
+    BucketKeyStatus,
+  };
+
+  class Configuration
+  {
+  public:
+    std::string id; // unique identifier
+
+    class Filter
+    {
+    public:
+      std::string filter_prefix; // the only defined filter, as yet
+    } filter;
+
+    class Destination
+    {
+    public:
+      Format format;
+      std::string account_id;
+      std::string bucket_arn;
+      std::string prefix;
+
+      class Encryption
+      {
+	std::string key_id; // for SSE-KMS; SSE-S3 exists but is undefined
+      };
+    };
+
+    Schedule schedule;
+    ObjectVersions versions;
+    uint32_t optional_fields; // bitmap
+
+    void dump_xml(Formatter *f) const;
+    void decode_xml(XMLObj *obj);
+  };
+
+  class InventoryConfigurations
+  {
+  public:
+    std::map<std::string, Configuration> id_mapping;
+
+    void dump_xml(Formatter *f) const;
+    void decode_xml(XMLObj *obj);
+  };
+
+}} /* namespace rgw::inv */
