07049ef7341e9e59026116cd95251bbbc8bc35f7
Matt Benjamin <mbenjamin@redhat.com>
2022-01-22 20:34:45 -0500

    rgwlc:  stage rgw_inventory.{h,cc}
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

---
+++ src/rgw/rgw_inventory.h
@@ -0,0 +1,15 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab ft=cpp
+
+#pragma once
+
+#include <map>
+#include <string>
+#include <iostream>
+#include <include/types.h>
+
+#include "include/str_list.h"
+#include "rgw_lc.h"
+#include "rgw_xml.h"
+
+
