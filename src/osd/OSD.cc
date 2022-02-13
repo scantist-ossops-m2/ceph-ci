573d0cfc8ec1b4444bbed2a1f3f4ee2bd00cceef
Omri Zeneva <ozeneva@redhat.com>
2022-02-13 20:58:39 +0200

    osd: convert tracer to be thread-local
    Signed-off-by: Omri Zeneva <ozeneva@redhat.com>

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -3450,7 +3450,6 @@ int OSD::init()
   std::lock_guard lock(osd_lock);
   if (is_stopping())
     return 0;
-  tracing::osd::tracer.init("osd");
   tick_timer.init();
   tick_timer_without_osd_lock.init();
   service.recovery_request_timer.init();
@@ -4435,7 +4434,6 @@ int OSD::shutdown()
   hb_front_server_messenger->shutdown();
   hb_back_server_messenger->shutdown();
 
-  tracing::osd::tracer.shutdown();
 
   return r;
 }
