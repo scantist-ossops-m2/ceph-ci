mkogan1/ceph

mgr/dashboard: branding: Replace Upstream Urls pacific
Resolves: rhbz#1865820

Signed-off-by: Nizamudeen A <nia@redhat.com>

src/pybind/mgr/dashboard/frontend/src/app/shared/services/doc.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 
 import { BehaviorSubject, Subscription } from 'rxjs';
 import { filter, first, map } from 'rxjs/operators';
-
 import { CephReleaseNamePipe } from '../pipes/ceph-release-name.pipe';
 import { SummaryService } from './summary.service';
 
@@ -10,7 +9,7 @@ import { SummaryService } from './summary.service';
   providedIn: 'root'
 })
 export class DocService {
-  private releaseDataSource = new BehaviorSubject<string>(null);
+  private releaseDataSource = new BehaviorSubject<string>('5');
   releaseData$ = this.releaseDataSource.asObservable();
 
   constructor(
@@ -23,20 +22,20 @@ export class DocService {
     });
   }
 
-  urlGenerator(section: string, release = 'master'): string {
-    const domain = `http://docs.ceph.com/docs/${release}/`;
+  urlGenerator(section: string, release?: string): string {
+    const domain = `https://access.redhat.com/documentation/en-us/red_hat_ceph_storage/${release}/html/`;
     const domainCeph = `https://ceph.io/`;
 
     const sections = {
-      iscsi: `${domain}mgr/dashboard/#enabling-iscsi-management`,
-      prometheus: `${domain}mgr/dashboard/#enabling-prometheus-alerting`,
-      'nfs-ganesha': `${domain}mgr/dashboard/#configuring-nfs-ganesha-in-the-dashboard`,
-      'rgw-nfs': `${domain}radosgw/nfs`,
-      rgw: `${domain}mgr/dashboard/#enabling-the-object-gateway-management-frontend`,
-      dashboard: `${domain}mgr/dashboard`,
-      grafana: `${domain}mgr/dashboard/#enabling-the-embedding-of-grafana-dashboards`,
-      orch: `${domain}mgr/orchestrator`,
-      pgs: `${domainCeph}pgcalc`,
+      iscsi: `${domain}dashboard_guide/block_devices#iscsi-functions`,
+      prometheus: `${domain}dashboard_guide/managing-the-cluster#viewing-and-managing-alerts`,
+      'nfs-ganesha': `${domain}dashboard_guide/object-gateway#nfs-ganesha`,
+      'rgw-nfs': `${domain}object_gateway_configuration_and_administration_guide/rgw-configuration-rgw#exporting-the-namespace-to-nfs-ganesha-rgw`,
+      rgw: `${domain}dashboard_guide/object-gateway`,
+      dashboard: `${domain}dashboard_guide/`,
+      grafana: `${domain}dashboard_guide/managing-the-cluster#managing-the-prometheus-environment_dash`,
+      orch: `${domain}operations_guide/orchestrator/`,
+      pgs: `https://access.redhat.com/labs/cephpgc/`,
       help: `${domainCeph}help/`,
       security: `${domainCeph}security/`,
       trademarks: `${domainCeph}legal-page/trademarks/`,

scantist-ossops-m2/ceph-ci

src/pybind/mgr/dashboard/frontend/src/app/shared/services/doc.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 
 import { BehaviorSubject, Subscription } from 'rxjs';
 import { filter, first, map } from 'rxjs/operators';
-
 import { CephReleaseNamePipe } from '../pipes/ceph-release-name.pipe';
 import { SummaryService } from './summary.service';
 
@@ -10,7 +9,7 @@ import { SummaryService } from './summary.service';
   providedIn: 'root'
 })
 export class DocService {
-  private releaseDataSource = new BehaviorSubject<string>(null);
+  private releaseDataSource = new BehaviorSubject<string>('5');
   releaseData$ = this.releaseDataSource.asObservable();
 
   constructor(
@@ -25,19 +24,19 @@ export class DocService {
 
   urlGenerator(section: string, release = 'master'): string {
     const docVersion = release === 'master' ? 'latest' : release;
-    const domain = `https://docs.ceph.com/en/${docVersion}/`;
+    const domain = `https://access.redhat.com/documentation/en-us/red_hat_ceph_storage/${docVersion}/html/`;
     const domainCeph = `https://ceph.io/`;
 
     const sections = {
-      iscsi: `${domain}mgr/dashboard/#enabling-iscsi-management`,
-      prometheus: `${domain}mgr/dashboard/#enabling-prometheus-alerting`,
-      'nfs-ganesha': `${domain}mgr/dashboard/#configuring-nfs-ganesha-in-the-dashboard`,
-      'rgw-nfs': `${domain}radosgw/nfs`,
-      rgw: `${domain}mgr/dashboard/#enabling-the-object-gateway-management-frontend`,
-      dashboard: `${domain}mgr/dashboard`,
-      grafana: `${domain}mgr/dashboard/#enabling-the-embedding-of-grafana-dashboards`,
-      orch: `${domain}mgr/orchestrator`,
-      pgs: `${domainCeph}pgcalc`,
+      iscsi: `${domain}dashboard_guide/block_devices#iscsi-functions`,
+      prometheus: `${domain}dashboard_guide/managing-the-cluster#viewing-and-managing-alerts`,
+      'nfs-ganesha': `${domain}dashboard_guide/object-gateway#nfs-ganesha`,
+      'rgw-nfs': `${domain}object_gateway_configuration_and_administration_guide/rgw-configuration-rgw#exporting-the-namespace-to-nfs-ganesha-rgw`,
+      rgw: `${domain}dashboard_guide/object-gateway`,
+      dashboard: `${domain}dashboard_guide/`,
+      grafana: `${domain}dashboard_guide/managing-the-cluster#managing-the-prometheus-environment_dash`,
+      orch: `${domain}operations_guide/orchestrator/`,
+      pgs: `https://access.redhat.com/labs/cephpgc/`,
       help: `${domainCeph}help/`,
       security: `${domainCeph}security/`,
       trademarks: `${domainCeph}legal-page/trademarks/`,