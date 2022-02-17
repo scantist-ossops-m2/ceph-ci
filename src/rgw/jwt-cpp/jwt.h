mkogan1/ceph

rgw/sts: code to fix compilation error due to RSA_set0_key in el7.
Fixes: rhbz#1966849

Signed-off-by: Pritha Srivastava <prsrivas@redhat.com>

src/rgw/jwt-cpp/jwt.h
@@ -331,7 +331,13 @@ namespace jwt {
 				if (e && n) {
 					EVP_PKEY* pRsaKey = EVP_PKEY_new();
 					RSA* rsa = RSA_new();
+					#if (OPENSSL_VERSION_NUMBER < 0x10100000L)
+					rsa->n = n;
+					rsa->e = e;
+					rsa->d = d;
+					#else
 					RSA_set0_key(rsa, n, e, nullptr);
+					#endif
 					EVP_PKEY_assign_RSA(pRsaKey, rsa);
 					shared_ptr<EVP_PKEY> p(pRsaKey, EVP_PKEY_free);
 					pkey = p;

scantist-ossops-m2/ceph-ci

Fixes: rhbz#2006178
(cherry picked from commit d08c252e7502e2b655328a63264f9b6d9ae91ae8)

src/rgw/jwt-cpp/jwt.h
@@ -331,7 +331,13 @@ namespace jwt {
 				if (e && n) {
 					EVP_PKEY* pRsaKey = EVP_PKEY_new();
 					RSA* rsa = RSA_new();
+					#if (OPENSSL_VERSION_NUMBER < 0x10100000L)
+					rsa->n = n;
+					rsa->e = e;
+					rsa->d = nullptr;
+					#else
 					RSA_set0_key(rsa, n, e, nullptr);
+					#endif
 					EVP_PKEY_assign_RSA(pRsaKey, rsa);
 					shared_ptr<EVP_PKEY> p(pRsaKey, EVP_PKEY_free);
 					pkey = p;