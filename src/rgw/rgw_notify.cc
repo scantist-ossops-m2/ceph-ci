ceph/ceph

rgw/sts: code changes to store multi-valued tags for objects and buckets (to be used as s3:ResourceTags in Identity and Resource policies).
Test code changes as suggested by Yuval Lifshitz.

Signed-off-by: Pritha Srivastava <prsrivas@redhat.com>

src/rgw/rgw_notify.cc
@@ -646,7 +646,7 @@ void metadata_from_attributes(const req_state* s, rgw::sal::Object* obj, KeyValu
   }
 }
 
-void tags_from_attributes(const req_state* s, rgw::sal::Object* obj, KeyValueMap& tags) {
+void tags_from_attributes(const req_state* s, rgw::sal::Object* obj, KeyMultiValueMap& tags) {
   const auto src_obj = get_object_with_atttributes(s, obj);
   if (!src_obj) {
     return;
@@ -748,7 +748,7 @@ bool notification_match(reservation_t& res, const rgw_pubsub_topic_filter& filte
       }
     } else {
       // try to fetch tags from the attributes
-      KeyValueMap tags;
+      KeyMultiValueMap tags;
       tags_from_attributes(s, obj, tags);
       if (!match(filter.s3_filter.tag_filter, tags)) {
         return false;

scantist-ossops-m2/ceph-ci

fixes: rhbz#2006174
(cherry picked from commit b682db4f6cf9aba4b09d8b323a6593c65c20eff1)

src/rgw/rgw_notify.cc
@@ -646,7 +646,7 @@ void metadata_from_attributes(const req_state* s, rgw::sal::RGWObject* obj, KeyV
   }
 }
 
-void tags_from_attributes(const req_state* s, rgw::sal::RGWObject* obj, KeyValueMap& tags) {
+void tags_from_attributes(const req_state* s, rgw::sal::RGWObject* obj, KeyMultiValueMap& tags) {
   const auto src_obj = get_object_with_atttributes(s, obj);
   if (!src_obj) {
     return;
@@ -748,7 +748,7 @@ bool notification_match(reservation_t& res, const rgw_pubsub_topic_filter& filte
       }
     } else {
       // try to fetch tags from the attributes
-      KeyValueMap tags;
+      KeyMultiValueMap tags;
       tags_from_attributes(s, obj, tags);
       if (!match(filter.s3_filter.tag_filter, tags)) {
         return false;