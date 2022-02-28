34cb9a2d8c26c5e226ec98564809a09f5a9d689d
Pere Diaz Bou <pdiazbou@redhat.com>
2022-02-28 13:58:25 +0100

    exporter: socket magic
    Signed-off-by: Pere Diaz Bou <pdiazbou@redhat.com>
    Co-authored-by: Avan Thakkar <athakkar@redhat.com>

--- src/mgr/DaemonMetricCollector.cc
+++ src/mgr/DaemonMetricCollector.cc
@@ -1,7 +1,40 @@
 #include "DaemonMetricCollector.h"
-#include <iostream>
+#include "common/admin_socket_client.h"
 
+#include <iostream>
+#include <string>
+#include <filesystem>
 
 void DaemonMetricCollector::main() {
   std::cout << "metric" << std::endl;
+  send_request_per_client();
+  // start server
+  while (1) {
+    // request_perfcounters();
+    // 1. updates sockets
+    // 1.1 list dir
+    // 1.2 updates vector
+    // 2. send request per client
+    // 3. update global perf counters
+    update_sockets();
+   }
+}
+
+void update_sockets() {
+  for (const auto &entry : fs::recursive_directory_iterator(socketdir)) {
+    if (entry.path().extension() == ".asok") {
+      if (clients.find(entry.path()) == clients.end()) {
+      AdminSocketClient sock(entry.path());
+      clients[entry.path()] = sock;
+    }
+    }
+  }
+}
+
+void DaemonMetricCollector::send_request_per_client() {
+  AdminSocketClient mgr_sock_client("/var/run/ceph/whatever");
+  std::string request("{\"prefix\":\"perf dump\"}");
+  std::string response;
+  mgr_sock_client.do_request(request, &response);
+  std::cout << response << std::endl;
 }
--- src/mgr/DaemonMetricCollector.h
+++ src/mgr/DaemonMetricCollector.h
@@ -1,7 +1,25 @@
 #pragma once
+#include "common/admin_socket_client.h"
+#include <filesystem>
+#include <string>
+#include <map>
+#include <vector>
+
+namespace fs = std::filesystem;
 
 class DaemonMetricCollector {
  public:
   int i;
   void main();
+
+private:
+  // TODO: add clients
+  //       check removed sockets
+  //       list dir of sockets
+  std::map<std::string, AdminSocketClient> clients;
+  fs::path socketdir = "/var/run/ceph";
+  void update_sockets();
+  void send_request_per_client();
+  // void transform_perf_data();
+  // int start_prometheus_server();
 };
