3cd74906555de1eccd789a003c5f9fe999e166f8
John Mulligan <jmulligan@redhat.com>
2022-04-09 14:29:25 -0400

    pybind/mgr: add ObjectFormatAdapter type to object_format.py
    The ObjectFormatAdapter fills the role for bridging between types
    that can return a simplified representation of themselves and
    actually formatting objects as JSON and YAML.
    
    Note that we do not want generally want types that serialize themselves
    to JSON/YAML strings. That approach makes it harder to standardize on
    the final output formatting (indentation, multiple yaml docs, etc).
    Additionally, we do not want the types to need to specialize between
    JSON and YAML. So, by default, we try to use a method `to_simplified`
    which is not specific to any serialization format.  However, for
    backwards compatibility with types that already have methods *that
    return dicts/lists/etc* under the names `to_json` or `to_yaml` we
    support using the `compatible` flag to enable the use of those methods.
    If the adaptor fails to find a conversion method on the object,
    serialization of the object itself is attempted - this way return values
    of simple lists, dicts, etc also works.
    
    An earlier version of this patch tried to share the JSON/YAML
    serialization logic found in src/pybind/mgr/orchestrator/module.py.
    However, this approach was deemed too complicated and we also preferred
    to use yaml safe dumping whenever possible.  This does lead to a level
    of code duplication. Dealing with this duplication is a task left for
    the future.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- src/pybind/mgr/object_format.py
+++ src/pybind/mgr/object_format.py
@@ -2,6 +2,26 @@
 # requested output formats such as JSON, YAML, etc.
 
 import enum
+import json
+import sys
+
+from typing import (
+    Any,
+    Dict,
+    Optional,
+)
+
+import yaml
+
+# this uses a version check as opposed to a try/except because this
+# form makes mypy happy and try/except doesn't.
+if sys.version_info >= (3, 8):
+    from typing import runtime_checkable, Protocol
+else:
+    from typing_extensions import runtime_checkable, Protocol
+
+
+DEFAULT_JSON_INDENT: int = 2
 
 
 class Format(enum.Enum):
@@ -11,3 +31,110 @@ class Format(enum.Enum):
     yaml = "yaml"
     xml_pretty = "xml-pretty"
     xml = "xml"
+
+
+# SimpleData is a type alias for Any unless we can determine the
+# exact set of subtypes we want to support. But it is explicit!
+SimpleData = Any
+
+
+@runtime_checkable
+class SimpleDataProvider(Protocol):
+    def to_simplified(self) -> SimpleData:
+        """Return a simplified representation of the current object.
+        The simplified representation should be trivially serializable.
+        """
+        ...  # pragma: no cover
+
+
+@runtime_checkable
+class JSONDataProvider(Protocol):
+    def to_json(self) -> Any:
+        """Return a python object that can be serialized into JSON.
+        This function does _not_ return a JSON string.
+        """
+        ...  # pragma: no cover
+
+
+@runtime_checkable
+class YAMLDataProvider(Protocol):
+    def to_yaml(self) -> Any:
+        """Return a python object that can be serialized into YAML.
+        This function does _not_ return a string of YAML.
+        """
+        ...  # pragma: no cover
+
+
+class JSONFormatter(Protocol):
+    def format_json(self) -> str:
+        """Return a JSON formatted representation of an object."""
+        ...  # pragma: no cover
+
+
+class YAMLFormatter(Protocol):
+    def format_yaml(self) -> str:
+        """Return a JSON formatted representation of an object."""
+        ...  # pragma: no cover
+
+
+class ObjectFormatAdapter:
+    """A format adapater for a single object.
+    Given an input object, this type will adapt the object, or a simplified
+    representation of the object, to either JSON or YAML when the format_json or
+    format_yaml methods are used.
+
+    If the compatible flag is true and the object provided to the adapter has
+    methods such as `to_json` and/or `to_yaml` these methods will be called in
+    order to get a JSON/YAML compatible simplified representation of the
+    object.
+
+    If the above case is not satisfied and the object provided to the adapter
+    has a method `to_simplified`, this method will be called to acquire a
+    simplified representation of the object.
+
+    If none of the above cases is true, the object itself will be used for
+    serialization. If the object can not be safely serialized an exception will
+    be raised.
+
+    NOTE: Some code may use methods named like `to_json` to return a JSON
+    string. If that is the case, you should not use that method with the
+    ObjectFormatAdapter. Do not set compatible=True for objects of this type.
+    """
+
+    def __init__(
+        self,
+        obj: Any,
+        json_indent: Optional[int] = DEFAULT_JSON_INDENT,
+        compatible: bool = False,
+    ) -> None:
+        self.obj = obj
+        self._compatible = compatible
+        self.json_indent = json_indent
+
+    def _fetch_json_data(self) -> Any:
+        # if the data object provides a specific simplified representation for
+        # JSON (and compatible mode is enabled) get the data via that method
+        if self._compatible and isinstance(self.obj, JSONDataProvider):
+            return self.obj.to_json()
+        # otherwise we use our specific method `to_simplified` if it exists
+        if isinstance(self.obj, SimpleDataProvider):
+            return self.obj.to_simplified()
+        # and fall back to the "raw" object
+        return self.obj
+
+    def format_json(self) -> str:
+        """Return a JSON formatted string representing the input object."""
+        return json.dumps(
+            self._fetch_json_data(), indent=self.json_indent, sort_keys=True
+        )
+
+    def _fetch_yaml_data(self) -> Any:
+        if self._compatible and isinstance(self.obj, YAMLDataProvider):
+            return self.obj.to_yaml()
+        # nothing specific to YAML was found. use the simplified representation
+        # for JSON, as all valid JSON is valid YAML.
+        return self._fetch_json_data()
+
+    def format_yaml(self) -> str:
+        """Return a YAML formatted string representing the input object."""
+        return yaml.safe_dump(self._fetch_yaml_data())
