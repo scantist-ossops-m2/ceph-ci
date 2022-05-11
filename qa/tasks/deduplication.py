fb2b7b9ea2d19a87fdfbf98fba867f5527e7ccb7
myoungwon oh <ohmyoungwon@gmail.com>
2022-05-11 21:21:05 +0900

    qa: add deduplication test under RGW workload (s3)
    Signed-off-by: Myoungwon Oh <myoungwon.oh@samsung.com>

---
+++ qa/tasks/deduplication.py
@@ -0,0 +1,204 @@
+"""
+Run ceph-dedup-tool
+"""
+import contextlib
+import logging
+import gevent
+from teuthology import misc as teuthology
+
+
+from teuthology.orchestra import run
+
+log = logging.getLogger(__name__)
+
+@contextlib.contextmanager
+def task(ctx, config):
+    """
+    Run ceph-dedup-tool.
+
+    The config should be as follows::
+
+        ceph-dedup-tool:
+          clients: [client list]
+          op: <operation name>
+          object: <object name>
+          max_in_flight: <max number of operations in flight>
+          object_size: <size of objects in bytes>
+          min_stride_size: <minimum write stride size in bytes>
+          max_stride_size: <maximum write stride size in bytes>
+          op_weights: <dictionary mapping operation type to integer weight>
+          runs: <number of times to run> - the pool is remade between runs
+          ec_pool: use an ec pool
+          erasure_code_profile: profile to use with the erasure coded pool
+          fast_read: enable ec_pool's fast_read
+          min_size: set the min_size of created pool
+          pool_snaps: use pool snapshots instead of selfmanaged snapshots
+	  write_fadvise_dontneed: write behavior like with LIBRADOS_OP_FLAG_FADVISE_DONTNEED.
+	                          This mean data don't access in the near future.
+				  Let osd backend don't keep data in cache.
+
+    For example::
+
+        tasks:
+        - ceph:
+        - rados:
+            clients: [client.0]
+            ops: 1000
+            max_seconds: 0   # 0 for no limit
+            objects: 25
+            max_in_flight: 16
+            object_size: 4000000
+            min_stride_size: 1024
+            max_stride_size: 4096
+            op_weights:
+              read: 20
+              write: 10
+              delete: 2
+              snap_create: 3
+              rollback: 2
+              snap_remove: 0
+            ec_pool: create an ec pool, defaults to False
+            erasure_code_use_overwrites: test overwrites, default false
+            erasure_code_profile:
+              name: teuthologyprofile
+              k: 2
+              m: 1
+              crush-failure-domain: osd
+            pool_snaps: true
+	    write_fadvise_dontneed: true
+            runs: 10
+        - interactive:
+
+    Optionally, you can provide the pool name to run against:
+
+        tasks:
+        - ceph:
+        - exec:
+            client.0:
+              - ceph osd pool create foo
+        - rados:
+            clients: [client.0]
+            pools: [foo]
+            ...
+
+    Alternatively, you can provide a pool prefix:
+
+        tasks:
+        - ceph:
+        - exec:
+            client.0:
+              - ceph osd pool create foo.client.0
+        - rados:
+            clients: [client.0]
+            pool_prefix: foo
+            ...
+
+    The tests are run asynchronously, they are not complete when the task
+    returns. For instance:
+
+        - rados:
+            clients: [client.0]
+            pools: [ecbase]
+            ops: 4000
+            objects: 500
+            op_weights:
+              read: 100
+              write: 100
+              delete: 50
+              copy_from: 50
+        - print: "**** done rados ec-cache-agent (part 2)"
+
+     will run the print task immediately after the rados tasks begins but
+     not after it completes. To make the rados task a blocking / sequential
+     task, use:
+
+        - sequential:
+          - rados:
+              clients: [client.0]
+              pools: [ecbase]
+              ops: 4000
+              objects: 500
+              op_weights:
+                read: 100
+                write: 100
+                delete: 50
+                copy_from: 50
+        - print: "**** done rados ec-cache-agent (part 2)"
+
+    """
+    log.info('Beginning deduplication...')
+    assert isinstance(config, dict), \
+        "please list clients to run on"
+
+    #assert hasattr(ctx, 'rgw')
+    """
+    object_size = int(config.get('object_size', 4000000))
+    op_weights = config.get('op_weights', {})
+    """
+    testdir = teuthology.get_testdir(ctx)
+    args = [
+        'adjust-ulimits',
+        'ceph-coverage',
+        '{tdir}/archive/coverage'.format(tdir=testdir),
+        'ceph-dedup-tool']
+    if config.get('op', None):
+        args.extend(['--op', config.get('op', None)])
+    if config.get('chunk_pool', None):
+        args.extend(['--chunk-pool', config.get('chunk_pool', None)])
+    if config.get('chunk_size', False):
+        args.extend(['--chunk-size', str(config.get('chunk_size', 8192))])
+    if config.get('chunk_algorithm', False):
+        args.extend(['--chunk-algorithm', config.get('chunk_algorithm', None)] )
+    if config.get('fingerprint_algorithm', False):
+        args.extend(['--fingerprint-algorithm', config.get('fingerprint_algorithm', None)] )
+    if config.get('object_dedup_threshold', False):
+        args.extend(['--object-dedup-threshold', str(config.get('object_dedup_threshold', 50))])
+    if config.get('chunk_dedup_threshold', False):
+        args.extend(['--chunk-dedup-threshold', str(config.get('chunk_dedup_threshold', 5))])
+    if config.get('max_thread', False):
+        args.extend(['--max-thread', str(config.get('max_thread', 2))])
+    if config.get('pool', False):
+        args.extend(['--pool', config.get('pool', None)])
+
+    args.extend([
+        '--debug',
+        '--deamon',
+        '--iterative'])
+    """
+    args.extend([
+        '--max-ops', str(config.get('ops', 10000)),
+        '--objects', str(config.get('objects', 500)),
+        '--max-in-flight', str(config.get('max_in_flight', 16)),
+        '--size', str(object_size),
+        '--min-stride-size', str(config.get('min_stride_size', object_size // 10)),
+        '--max-stride-size', str(config.get('max_stride_size', object_size // 5)),
+        '--max-seconds', str(config.get('max_seconds', 0))
+        ])
+    """
+
+    def thread():
+        clients = ['client.{id}'.format(id=id_) for id_ in teuthology.all_roles_of_type(ctx.cluster, 'client')]
+        log.info('clients are %s' % clients)
+        manager = ctx.managers['ceph']
+        tests = {}
+        log.info("args %s", args)
+        for role in config.get('clients', clients):
+            assert isinstance(role, str)
+            PREFIX = 'client.'
+            assert role.startswith(PREFIX)
+            id_ = role[len(PREFIX):]
+            (remote,) = ctx.cluster.only(role).remotes.keys()
+            proc = remote.run(
+                args=args,
+                stdin=run.PIPE,
+                wait=False
+                )
+            tests[id_] = proc
+
+    running = gevent.spawn(thread)
+
+    try:
+        yield
+    finally:
+        log.info('joining ceph-dedup-tool')
+        running.get()
