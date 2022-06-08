ceph/ceph

rgw: rename rgw_data_change::gen_id
Signed-off-by: Casey Bodley <cbodley@redhat.com>

src/rgw/rgw_data_sync.cc
@@ -1735,7 +1735,7 @@ public:
           if (!marker_tracker->start(log_iter->log_id, 0, log_iter->log_timestamp)) {
             tn->log(0, SSTR("ERROR: cannot start syncing " << log_iter->log_id << ". Duplicate entry?"));
           } else {
-            spawn(sync_single_entry(source_bs, std::nullopt, log_iter->log_id,
+            spawn(sync_single_entry(source_bs, log_iter->entry.gen, log_iter->log_id,
                                     log_iter->log_timestamp, false), false);
           }
 

scantist-ossops-m2/ceph-ci

src/rgw/rgw_data_sync.cc
@@ -1733,7 +1733,7 @@ public:
           if (!marker_tracker->start(log_iter->log_id, 0, log_iter->log_timestamp)) {
             tn->log(0, SSTR("ERROR: cannot start syncing " << log_iter->log_id << ". Duplicate entry?"));
           } else {
-            yield_spawn_window(sync_single_entry(source_bs, std::nullopt, log_iter->log_id,
+            yield_spawn_window(sync_single_entry(source_bs, log_iter->entry.gen, log_iter->log_id,
                                                  log_iter->log_timestamp, false),
                                cct->_conf->rgw_data_sync_spawn_window, std::nullopt);
           }