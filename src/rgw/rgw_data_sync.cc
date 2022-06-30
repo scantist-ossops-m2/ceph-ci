659b4c819f24149c7f05db00e6edcf3aba3a3f06
Adam C. Emerson <aemerson@redhat.com>
2022-06-30 13:48:49 -0400

    rgw: RGWBucketPipeSyncStatusManger takes optional ostream
    `radosgw-admin` provides it when the `--extra-info` option is provided.
    
    It gets stashed in the SyncEnv for later use.
    
    Signed-off-by: Adam C. Emerson <aemerson@redhat.com>
    (cherry picked from commit 702c03c9c841a20899c40d1f7cb81e09cf7227c7)
    
    Conflicts:
    	src/rgw/rgw_admin.cc
    	src/rgw/rgw_data_sync.h
     - Upstream SAL
    
    Resolves: rhbz#2025932
    Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

--- src/rgw/rgw_data_sync.cc
+++ src/rgw/rgw_data_sync.cc
@@ -5346,7 +5346,8 @@ int RGWSyncBucketCR::operate(const DoutPrefixProvider *dpp)
   return 0;
 }
 
-int RGWBucketPipeSyncStatusManager::do_init(const DoutPrefixProvider *dpp)
+int RGWBucketPipeSyncStatusManager::do_init(const DoutPrefixProvider *dpp,
+					    std::ostream* ostr)
 {
   int ret = http_manager.start();
   if (ret < 0) {
@@ -5361,6 +5362,8 @@ int RGWBucketPipeSyncStatusManager::do_init(const DoutPrefixProvider *dpp)
                 store->svc(), async_rados, &http_manager,
                 error_logger.get(), store->getRados()->get_sync_tracer(),
                 sync_module, nullptr);
+
+  sync_env.ostr = ostr;
 
   rgw_sync_pipe_info_set pipes;
 
@@ -5439,12 +5442,13 @@ RGWBucketPipeSyncStatusManager::construct(
   rgw::sal::RGWRadosStore* store,
   std::optional<rgw_zone_id> source_zone,
   std::optional<rgw_bucket> source_bucket,
-  const rgw_bucket& dest_bucket)
+  const rgw_bucket& dest_bucket,
+  std::ostream* ostr)
 {
   std::unique_ptr<RGWBucketPipeSyncStatusManager> self{
     new RGWBucketPipeSyncStatusManager(store, source_zone, source_bucket,
 				       dest_bucket)};
-  auto r = self->do_init(dpp);
+  auto r = self->do_init(dpp, ostr);
   if (r < 0) {
     return tl::unexpected(r);
   }
