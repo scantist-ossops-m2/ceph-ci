ceph/ceph

rgw: fix segfault in OpsLogRados::log when realm is reloaded
We weren't previously handling the deallocation of the store when
a realm was reloaded. Now passing a const reference to the pointer.

Fixes: https://tracker.ceph.com/issues/54130
Signed-off-by: Cory Snyder <csnyder@iland.com>

src/rgw/rgw_log.h
@@ -199,9 +199,11 @@ public:
 };
 
 class OpsLogRados : public OpsLogSink {
-  rgw::sal::Store* store;
+  // main()'s Store pointer as a reference, possibly modified by RGWRealmReloader
+  rgw::sal::Store* const& store;
+
 public:
-  OpsLogRados(rgw::sal::Store* store);
+  OpsLogRados(rgw::sal::Store* const& store);
   int log(struct req_state* s, struct rgw_log_entry& entry) override;
 };
 

scantist-ossops-m2/ceph-ci

(cherry picked from commit 0713f65355586b2f6ceeb6bbce8763158847e5ed)
Conflicts:
	src/rgw/rgw_log.cc
	src/rgw/rgw_log.h
Cherry-pick notes:
- conflicts due to rename of RGWRados to rgw::sal::Store after Pacific
(cherry picked from commit 44a37dc0023a69da33620dc7b6e112221f7a6a0b)

src/rgw/rgw_log.h
@@ -200,9 +200,10 @@ public:
 };
 
 class OpsLogRados : public OpsLogSink {
-  RGWRados* store;
+  // main()'s Store pointer as a reference, possibly modified by RGWRealmReloader
+  rgw::sal::RGWRadosStore* const& store;
 public:
-  OpsLogRados(RGWRados* store);
+  OpsLogRados(rgw::sal::RGWRadosStore* const& store);
   int log(struct req_state* s, struct rgw_log_entry& entry) override;
 };
 