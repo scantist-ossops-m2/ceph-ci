dang/ceph

RGW - Zipper - Add temporary Filter testing
In order to test filters, we need to be able to instantiate one and
stack it over a store.  Add a temporary config option to specify a
filter that can be used for testing.  This will be replaced with a
better system once the loadable module work is done.

Signed-off-by: Daniel Gryniewicz <dang@redhat.com>

src/rgw/librgw.cc
@@ -573,8 +573,16 @@ namespace rgw {
     }
 #endif
 
+    // Get the filter
+    std::string rgw_filter = "none";
+    const auto& config_filter = g_conf().get_val<std::string>("rgw_filter");
+    if (config_store == "base") {
+      rgw_filter = "base";
+    }
+
     store = StoreManager::get_storage(this, g_ceph_context,
 					 rgw_store,
+					 rgw_filter,
 					 run_gc,
 					 run_lc,
 					 run_quota,

src/rgw/rgw_admin.cc
@@ -4366,14 +4366,23 @@ int main(int argc, const char **argv)
     }
 #endif
 
+    // Get the filter
+    std::string rgw_filter = "none";
+    const auto& config_filter = g_conf().get_val<std::string>("rgw_filter");
+    if (config_store == "base") {
+      rgw_filter = "base";
+    }
+
     if (raw_storage_op) {
       store = StoreManager::get_raw_storage(dpp(),
 					    g_ceph_context,
-					    rgw_store);
+					    rgw_store,
+					    rgw_filter);
     } else {
       store = StoreManager::get_storage(dpp(),
 					g_ceph_context,
 					rgw_store,
+					rgw_filter,
 					false,
 					false,
 					false,

scantist-ossops-m2/ceph-ci

src/rgw/librgw.cc
@@ -573,8 +573,16 @@ namespace rgw {
     }
 #endif
 
+    // Get the filter
+    std::string rgw_filter = "none";
+    const auto& config_filter = g_conf().get_val<std::string>("rgw_filter");
+    if (config_filter == "base") {
+      rgw_filter = "base";
+    }
+
     store = StoreManager::get_storage(this, g_ceph_context,
 					 rgw_store,
+					 rgw_filter,
 					 run_gc,
 					 run_lc,
 					 run_quota,

src/rgw/rgw_admin.cc
@@ -4367,14 +4367,23 @@ int main(int argc, const char **argv)
     }
 #endif
 
+    // Get the filter
+    std::string rgw_filter = "none";
+    const auto& config_filter = g_conf().get_val<std::string>("rgw_filter");
+    if (config_filter == "base") {
+      rgw_filter = "base";
+    }
+
     if (raw_storage_op) {
       store = StoreManager::get_raw_storage(dpp(),
 					    g_ceph_context,
-					    rgw_store);
+					    rgw_store,
+					    rgw_filter);
     } else {
       store = StoreManager::get_storage(dpp(),
 					g_ceph_context,
 					rgw_store,
+					rgw_filter,
 					false,
 					false,
 					false,