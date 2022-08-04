a5aa0d6bdf04ef064d0d56669139ebd872ad29b7
Kefu Chai <tchaikov@gmail.com>
2022-08-04 13:09:11 +0800

    cmake: disable LTO when building pmdk
    See-also: https://tracker.ceph.com/issues/54473
    Signed-off-by: Kefu Chai <tchaikov@gmail.com>

--- cmake/modules/Buildpmdk.cmake
+++ cmake/modules/Buildpmdk.cmake
@@ -33,10 +33,11 @@ function(build_pmdk enable_ndctl)
   ExternalProject_Add(pmdk_ext
       ${source_dir_args}
       CONFIGURE_COMMAND ""
-      BUILD_COMMAND ${make_cmd} CC=${CMAKE_C_COMPILER} EXTRA_CFLAGS=-Wno-error NDCTL_ENABLE=${ndctl} BUILD_EXAMPLES=n BUILD_BENCHMARKS=n DOC=n
+      BUILD_COMMAND ${make_cmd} CC=${CMAKE_C_COMPILER} EXTRA_CFLAGS="-Wno-error -fno-lto" NDCTL_ENABLE=${ndctl} BUILD_EXAMPLES=n BUILD_BENCHMARKS=n DOC=n
       BUILD_IN_SOURCE 1
       BUILD_BYPRODUCTS "<SOURCE_DIR>/src/${PMDK_LIB_DIR}/libpmem.a" "<SOURCE_DIR>/src/${PMDK_LIB_DIR}/libpmemobj.a"
-      INSTALL_COMMAND "")
+      INSTALL_COMMAND ""
+      VERBATIM)
   unset(make_cmd)
 
   ExternalProject_Get_Property(pmdk_ext source_dir)
