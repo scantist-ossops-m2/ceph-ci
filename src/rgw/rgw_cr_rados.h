ceph/ceph

rgw: `SimpleRadosWriteCR` uses an async RADOS call
Don't go through the 'system object' cache. This also saves us the use
of the RADOS async completion processor.

Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

src/rgw/rgw_cr_rados.h
@@ -514,49 +514,59 @@ public:
 
 template <class T>
 class RGWSimpleRadosWriteCR : public RGWSimpleCoroutine {
-  const DoutPrefixProvider *dpp;
-  RGWAsyncRadosProcessor *async_rados;
-  RGWSI_SysObj *svc;
-  bufferlist bl;
+  const DoutPrefixProvider* dpp;
+  rgw::sal::RadosStore* const store;
   rgw_raw_obj obj;
-  RGWObjVersionTracker *objv_tracker;
+  RGWObjVersionTracker* objv_tracker;
   bool exclusive;
-  RGWAsyncPutSystemObj *req{nullptr};
+
+  bufferlist bl;
+  rgw_rados_ref ref;
+  std::map<std::string, bufferlist> unfiltered_attrs;
+  boost::intrusive_ptr<RGWAioCompletionNotifier> cn;
+
 
 public:
-  RGWSimpleRadosWriteCR(const DoutPrefixProvider *_dpp, 
-			RGWAsyncRadosProcessor *_async_rados, RGWSI_SysObj *_svc,
-			const rgw_raw_obj& _obj, const T& _data,
-			RGWObjVersionTracker *objv_tracker = nullptr,
+  RGWSimpleRadosWriteCR(const DoutPrefixProvider* dpp,
+			rgw::sal::RadosStore* const store,
+			rgw_raw_obj obj, const T& data,
+			RGWObjVersionTracker* objv_tracker = nullptr,
 			bool exclusive = false)
-    : RGWSimpleCoroutine(_svc->ctx()), dpp(_dpp), async_rados(_async_rados),
-      svc(_svc), obj(_obj), objv_tracker(objv_tracker), exclusive(exclusive) {
-    encode(_data, bl);
-  }
-
-  ~RGWSimpleRadosWriteCR() override {
-    request_cleanup();
-  }
-
-  void request_cleanup() override {
-    if (req) {
-      req->finish();
-      req = NULL;
-    }
+    : RGWSimpleCoroutine(store->ctx()), dpp(dpp), store(store),
+      obj(std::move(obj)), objv_tracker(objv_tracker), exclusive(exclusive) {
+    encode(data, bl);
   }
 
   int send_request(const DoutPrefixProvider *dpp) override {
-    req = new RGWAsyncPutSystemObj(dpp, this, stack->create_completion_notifier(),
-			           svc, objv_tracker, obj, exclusive, std::move(bl));
-    async_rados->queue(req);
-    return 0;
+    int r = store->getRados()->get_raw_obj_ref(dpp, obj, &ref);
+    if (r < 0) {
+      ldpp_dout(dpp, -1) << "ERROR: failed to get ref for (" << obj << ") ret="
+			 << r << dendl;
+      return r;
+    }
+
+    set_status() << "sending request";
+
+    librados::ObjectWriteOperation op;
+    if (exclusive) {
+      op.create(true);
+    }
+    if (objv_tracker) {
+      objv_tracker->prepare_op_for_write(&op);
+    }
+    op.write_full(bl);
+
+    cn = stack->create_completion_notifier();
+    return ref.pool.ioctx().aio_operate(ref.obj.oid, cn->completion(), &op);
   }
 
   int request_complete() override {
-    if (objv_tracker) { // copy the updated version
-      *objv_tracker = req->objv_tracker;
+    int ret = cn->completion()->get_return_value();
+    set_status() << "request complete; ret=" << ret;
+    if (ret >= 0 && objv_tracker) {
+      objv_tracker->apply_write();
     }
-    return req->get_ret_status();
+    return ret;
   }
 };
 

scantist-ossops-m2/ceph-ci

(cherry picked from commit b242879fc64f3870355fc2a20bd82e49b8ae6a99)
Conflicts:
	src/rgw/rgw_data_sync.cc
	src/rgw/rgw_sync.cc
	src/rgw/services/svc_mdlog.cc
 - Upstream SAL
Resolves: rhbz#2105324

src/rgw/rgw_cr_rados.h
@@ -514,49 +514,59 @@ public:
 
 template <class T>
 class RGWSimpleRadosWriteCR : public RGWSimpleCoroutine {
-  const DoutPrefixProvider *dpp;
-  RGWAsyncRadosProcessor *async_rados;
-  RGWSI_SysObj *svc;
-  bufferlist bl;
+  const DoutPrefixProvider* dpp;
+  rgw::sal::RGWRadosStore* const store;
   rgw_raw_obj obj;
-  RGWObjVersionTracker *objv_tracker;
+  RGWObjVersionTracker* objv_tracker;
   bool exclusive;
-  RGWAsyncPutSystemObj *req{nullptr};
+
+  bufferlist bl;
+  rgw_rados_ref ref;
+  std::map<std::string, bufferlist> unfiltered_attrs;
+  boost::intrusive_ptr<RGWAioCompletionNotifier> cn;
+
 
 public:
-  RGWSimpleRadosWriteCR(const DoutPrefixProvider *_dpp, 
-			RGWAsyncRadosProcessor *_async_rados, RGWSI_SysObj *_svc,
-			const rgw_raw_obj& _obj, const T& _data,
-			RGWObjVersionTracker *objv_tracker = nullptr,
+  RGWSimpleRadosWriteCR(const DoutPrefixProvider* dpp,
+			rgw::sal::RGWRadosStore* const store,
+			rgw_raw_obj obj, const T& data,
+			RGWObjVersionTracker* objv_tracker = nullptr,
 			bool exclusive = false)
-    : RGWSimpleCoroutine(_svc->ctx()), dpp(_dpp), async_rados(_async_rados),
-      svc(_svc), obj(_obj), objv_tracker(objv_tracker), exclusive(exclusive) {
-    encode(_data, bl);
-  }
-
-  ~RGWSimpleRadosWriteCR() override {
-    request_cleanup();
-  }
-
-  void request_cleanup() override {
-    if (req) {
-      req->finish();
-      req = NULL;
-    }
+    : RGWSimpleCoroutine(store->ctx()), dpp(dpp), store(store),
+      obj(std::move(obj)), objv_tracker(objv_tracker), exclusive(exclusive) {
+    encode(data, bl);
   }
 
   int send_request(const DoutPrefixProvider *dpp) override {
-    req = new RGWAsyncPutSystemObj(dpp, this, stack->create_completion_notifier(),
-			           svc, objv_tracker, obj, exclusive, std::move(bl));
-    async_rados->queue(req);
-    return 0;
+    int r = store->getRados()->get_raw_obj_ref(dpp, obj, &ref);
+    if (r < 0) {
+      ldpp_dout(dpp, -1) << "ERROR: failed to get ref for (" << obj << ") ret="
+			 << r << dendl;
+      return r;
+    }
+
+    set_status() << "sending request";
+
+    librados::ObjectWriteOperation op;
+    if (exclusive) {
+      op.create(true);
+    }
+    if (objv_tracker) {
+      objv_tracker->prepare_op_for_write(&op);
+    }
+    op.write_full(bl);
+
+    cn = stack->create_completion_notifier();
+    return ref.pool.ioctx().aio_operate(ref.obj.oid, cn->completion(), &op);
   }
 
   int request_complete() override {
-    if (objv_tracker) { // copy the updated version
-      *objv_tracker = req->objv_tracker;
+    int ret = cn->completion()->get_return_value();
+    set_status() << "request complete; ret=" << ret;
+    if (ret >= 0 && objv_tracker) {
+      objv_tracker->apply_write();
     }
-    return req->get_ret_status();
+    return ret;
   }
 };
 