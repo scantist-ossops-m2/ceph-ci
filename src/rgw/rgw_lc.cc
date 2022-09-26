ceph/ceph

rgwlc/sync: avoid calling merge-and-store-attrs from remove_bucket_config()
Calling merge-and-store attrs turns out to be unsafe from the context of
the metadata sync handler--although I am doubtful that really *should* be
the case.

Fixes: https://tracker.ceph.com/issues/56997

Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

src/rgw/rgw_lc.cc
@@ -2480,20 +2480,23 @@ int RGWLC::set_bucket_config(rgw::sal::Bucket* bucket,
 }
 
 int RGWLC::remove_bucket_config(rgw::sal::Bucket* bucket,
-                                const rgw::sal::Attrs& bucket_attrs)
+                                const rgw::sal::Attrs& bucket_attrs,
+				bool merge_attrs)
 {
   rgw::sal::Attrs attrs = bucket_attrs;
-  attrs.erase(RGW_ATTR_LC);
-  int ret = bucket->merge_and_store_attrs(this, attrs, null_yield);
-
   rgw_bucket& b = bucket->get_key();
+  int ret{0};
 
-  if (ret < 0) {
-    ldpp_dout(this, 0) << "RGWLC::RGWDeleteLC() failed to set attrs on bucket="
-        << b.name << " returned err=" << ret << dendl;
-    return ret;
+  if (merge_attrs) {
+    attrs.erase(RGW_ATTR_LC);
+    ret = bucket->merge_and_store_attrs(this, attrs, null_yield);
+
+    if (ret < 0) {
+      ldpp_dout(this, 0) << "RGWLC::RGWDeleteLC() failed to set attrs on bucket="
+			 << b.name << " returned err=" << ret << dendl;
+      return ret;
+    }
   }
-
 
   ret = guard_lc_modify(this, store, sal_lc.get(), b, cookie,
 			[&](rgw::sal::Lifecycle* sal_lc, const string& oid,

scantist-ossops-m2/ceph-ci

Resolves: rhbz#2095670
(cherry picked from commit 7dd245af49ce7d5c2d51ddf8f7890c128c32cd51)

src/rgw/rgw_lc.cc
@@ -2349,22 +2349,24 @@ int RGWLC::set_bucket_config(RGWBucketInfo& bucket_info,
 }
 
 int RGWLC::remove_bucket_config(RGWBucketInfo& bucket_info,
-                                const map<string, bufferlist>& bucket_attrs)
+                                const map<string, bufferlist>& bucket_attrs,
+				bool merge_attrs)
 {
   map<string, bufferlist> attrs = bucket_attrs;
-  attrs.erase(RGW_ATTR_LC);
-  int ret =
-    store->ctl()->bucket->set_bucket_instance_attrs(
-      bucket_info, attrs, &bucket_info.objv_tracker, null_yield, this);
-
   rgw_bucket& bucket = bucket_info.bucket;
+  int ret{0};
+  if (merge_attrs) {
+    attrs.erase(RGW_ATTR_LC);
+    ret =
+      store->ctl()->bucket->set_bucket_instance_attrs(
+	bucket_info, attrs, &bucket_info.objv_tracker, null_yield, this);
 
-  if (ret < 0) {
-    ldpp_dout(this, 0) << "RGWLC::RGWDeleteLC() failed to set attrs on bucket="
-        << bucket.name << " returned err=" << ret << dendl;
-    return ret;
+    if (ret < 0) {
+      ldpp_dout(this, 0) << "RGWLC::RGWDeleteLC() failed to set attrs on bucket="
+			 << bucket.name << " returned err=" << ret << dendl;
+      return ret;
+    }
   }
-
 
   ret = guard_lc_modify(this, store, sal_lc.get(), bucket, cookie,
 			[&](rgw::sal::Lifecycle* sal_lc, const string& oid,