ceph/ceph

rgw: Fix `rgw::sal::Bucket::empty` static method signatures
`unique_ptr` overload should take by reference.

Both should be const.

Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

src/rgw/rgw_sal.h
@@ -745,9 +745,9 @@ class Bucket {
     virtual bool versioning_enabled() = 0;
 
     /** Check if a Bucket pointer is empty */
-    static bool empty(Bucket* b) { return (!b || b->empty()); }
+    static bool empty(const Bucket* b) { return (!b || b->empty()); }
     /** Check if a Bucket unique pointer is empty */
-    static bool empty(std::unique_ptr<Bucket> b) { return (!b || b->empty()); }
+    static bool empty(const std::unique_ptr<Bucket>& b) { return (!b || b->empty()); }
     /** Clone a copy of this bucket.  Used when modification is necessary of the copy */
     virtual std::unique_ptr<Bucket> clone() = 0;
 

scantist-ossops-m2/ceph-ci

(cherry picked from commit b1d3e6c00674ebf6bde08968789a426d65db73d9)
Conflicts:
	src/rgw/rgw_sal.h
 - `unique_ptr` overload of empty
Fixes: https://tracker.ceph.com/issues/56586
(cherry picked from commit 95c6c2d209a877b934e05e727a181de8c68accd3)
Resolves: rhbz#2109256

src/rgw/rgw_sal.h
@@ -287,7 +287,11 @@ class RGWBucket {
       ent.convert(b);
     }
 
-    static bool empty(RGWBucket* b) { return (!b || b->empty()); }
+    /** Check if a Bucket pointer is empty */
+    static bool empty(const RGWBucket* b) { return (!b || b->empty()); }
+    /** Check if a Bucket unique pointer is empty */
+    static bool empty(const std::unique_ptr<RGWBucket>& b) { return (!b || b->empty()); }
+
     virtual std::unique_ptr<RGWBucket> clone() = 0;
 
     /* dang - This is temporary, until the API is completed */