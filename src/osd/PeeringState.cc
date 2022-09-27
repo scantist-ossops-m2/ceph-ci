f08111a1aabaa99f3985a78bd280ba0b6db781f2
Aishwarya Mathuria <amathuri@redhat.com>
2022-09-27 10:52:01 +0530

    Testing laggy state
    Signed-off-by: Aishwarya Mathuria <amathuri@redhat.com>

--- src/osd/PeeringState.cc
+++ src/osd/PeeringState.cc
@@ -1213,14 +1213,18 @@ void PeeringState::proc_lease_ack(int from, const pg_lease_ack_t& a)
 	  was_min = true;
 	}
 	acting_readable_until_ub[i] = a.readable_until_ub;
+        break;
       }
-      break;
     }
   }
   if (was_min) {
+    dout(20) << __func__ << " was_min true" << dendl;
+    dout(20) << __func__ << " readable_until = " << readable_until << dendl;
+    dout(20) << __func__ << " NOW: " << now << dendl;
     auto old_ru = readable_until;
     recalc_readable_until();
     if (now >= old_ru) {
+      dout(20) << __func__ << " IN IF" << dendl;
       pl->recheck_readable();
     }
   }
