df5f48022ffccda1a35889e4b376569077633846
Milind Changire <mchangir@redhat.com>
2022-10-07 09:28:02 +0530

    qa: add verbose flag for kernel build
    Signed-off-by: Milind Changire <mchangir@redhat.com>

--- qa/workunits/kernel_untar_build.sh
+++ qa/workunits/kernel_untar_build.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+# for kernel debugging
+mount -t debugfs none /sys/kernel/debug
+echo "file fs/ceph/* +pf" > /sys/kernel/debug/dynamic_debug/control
+
 wget -O linux.tar.gz http://download.ceph.com/qa/linux-5.4.tar.gz
 
 mkdir t
@@ -9,7 +13,7 @@ cd t
 tar xzf ../linux.tar.gz
 cd linux*
 make defconfig
-make -j`grep -c processor /proc/cpuinfo`
+make -j`grep -c processor /proc/cpuinfo` V=1
 cd ..
 if ! rm -rv linux* ; then
     echo "uh oh rm -r failed, it left behind:"
