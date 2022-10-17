vshankar/ceph

test: cleanup files/dirs for some tests
Otherwise subsequent tests that use the same file/dir name may fail
on ceph_mkdir() expecting success.

Signed-off-by: Venky Shankar <vshankar@redhat.com>

src/test/libcephfs/acl.cc
@@ -280,6 +280,9 @@ TEST(ACL, DefaultACL) {
 
   free(acl1_buf);
   free(acl2_buf);
+  ASSERT_EQ(ceph_rmdir(cmount, test_file1), 0);
+  ASSERT_EQ(ceph_rmdir(cmount, test_dir2), 0);
+  ASSERT_EQ(ceph_rmdir(cmount, test_dir1), 0);
   ceph_close(cmount, fd);
   ceph_shutdown(cmount);
 }

scantist-ossops-m2/ceph-ci

src/test/libcephfs/acl.cc
@@ -280,6 +280,9 @@ TEST(ACL, DefaultACL) {
 
   free(acl1_buf);
   free(acl2_buf);
+  ASSERT_EQ(ceph_unlink(cmount, test_file1), 0);
+  ASSERT_EQ(ceph_rmdir(cmount, test_dir2), 0);
+  ASSERT_EQ(ceph_rmdir(cmount, test_dir1), 0);
   ceph_close(cmount, fd);
   ceph_shutdown(cmount);
 }