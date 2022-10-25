cdf53aab111075128209dacc42fc666412f399e6
Xiubo Li <xiubli@redhat.com>
2022-10-25 14:56:07 +0800

    qa: make iozone.sh to specify dedicated test directory
    Signed-off-by: Xiubo Li <xiubli@redhat.com>

--- qa/workunits/suites/iozone.sh
+++ qa/workunits/suites/iozone.sh
@@ -2,6 +2,15 @@
 
 set -ex
 
+testdir='./'
+if [ $# -ge 1 ]
+then
+    testdir=$1
+fi
+pushd $testdir
+
 iozone -c -e -s 1024M -r 16K -t 1 -F f1 -i 0 -i 1
 iozone -c -e -s 1024M -r 1M -t 1 -F f2 -i 0 -i 1
 iozone -c -e -s 10240M -r 1M -t 1 -F f3 -i 0 -i 1
+
+popd
