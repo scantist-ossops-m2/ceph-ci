d2ef64f0200dc96e95c3ae044b1ad28bff3b1435
Matan Breizman <mbreizma@redhat.com>
2022-12-11 13:44:17 +0000

    crimson/osd/osd_operations/client_request: Skip do_recover_missing() on replica
    * assert internal_client_request is on primary since
      do_recover_missing is also called by internal requests.
    
    Signed-off-by: Matan Breizman <mbreizma@redhat.com>

--- src/crimson/osd/osd_operations/client_request_common.cc
+++ src/crimson/osd/osd_operations/client_request_common.cc
@@ -17,6 +17,7 @@ CommonClientRequest::do_recover_missing(
   Ref<PG>& pg, const hobject_t& soid)
 {
   eversion_t ver;
+  assert (pg->is_primary());
   logger().debug("{} check for recovery, {}", __func__, soid);
   if (!pg->is_unreadable_object(soid, &ver) &&
       !pg->is_degraded_or_backfilling_object(soid)) {
