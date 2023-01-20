mkogan1/ceph

rgw/multisite: object should generate version id on archive zone, even when versioning is disabled on primary
Fixes rhbz#1957088
Signed-off-by: Shilpa Jagannath <smanjara@redhat.com>

src/rgw/rgw_data_sync.cc
@@ -2948,9 +2948,11 @@ RGWCoroutine *RGWArchiveDataSyncModule::sync_object(RGWDataSyncCtx *sc, rgw_buck
   if (versioned_epoch.value_or(0) == 0) { /* force version if not set */
     versioned_epoch = 0;
     dest_key = key;
-    if (key.instance.empty()) {
-      sync_env->store->getRados()->gen_rand_obj_instance_name(&(*dest_key));
-    }
+  }
+
+  if (key.instance.empty()) {
+    dest_key = key;
+    sync_env->store->getRados()->gen_rand_obj_instance_name(&(*dest_key));
   }
 
   return new RGWObjFetchCR(sc, sync_pipe, key, dest_key, versioned_epoch, zones_trace);

scantist-ossops-m2/ceph-ci

src/rgw/driver/rados/rgw_data_sync.cc
@@ -2987,9 +2987,11 @@ RGWCoroutine *RGWArchiveDataSyncModule::sync_object(const DoutPrefixProvider *dp
   if (versioned_epoch.value_or(0) == 0) { /* force version if not set */
     versioned_epoch = 0;
     dest_key = key;
-    if (key.instance.empty()) {
-      sync_env->driver->getRados()->gen_rand_obj_instance_name(&(*dest_key));
-    }
+  }
+
+  if (key.instance.empty()) {
+    dest_key = key;
+    sync_env->driver->getRados()->gen_rand_obj_instance_name(&(*dest_key));
   }
 
   return new RGWObjFetchCR(sc, sync_pipe, key, dest_key, versioned_epoch, zones_trace);