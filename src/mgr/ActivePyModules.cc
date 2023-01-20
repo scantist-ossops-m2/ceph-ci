vshankar/ceph

mgr, pybind/mgr, mgr/stats: be resilient to offline rank0 MDS
Reregister the user queries during the rank0 MDS failover event
by calling listener.handle_query_updated(). This enables
`ceph fs perf stats` to receive the updated metrics after the
failover.

Fixes: https://tracker.ceph.com/issues/50033
Signed-off-by: Jos Collin <jcollin@redhat.com>

src/mgr/ActivePyModules.cc
@@ -1387,6 +1387,11 @@ void ActivePyModules::remove_mds_perf_query(MetricQueryID query_id)
   }
 }
 
+void ActivePyModules::reregister_mds_perf_queries()
+{
+  server.reregister_mds_perf_queries();
+}
+
 PyObject *ActivePyModules::get_mds_perf_counters(MetricQueryID query_id)
 {
   MDSPerfCollector collector(query_id);
@@ -1429,6 +1434,11 @@ PyObject *ActivePyModules::get_mds_perf_counters(MetricQueryID query_id)
     f.close_section(); // i
   }
   f.close_section(); // counters
+
+  f.open_array_section("last_updated");
+  f.dump_float("last_updated_ts", collector.last_updated);
+  f.close_section(); // last_updated
+
   f.close_section(); // metrics
 
   return f.get();

src/mgr/MDSPerfMetricTypes.h
@@ -317,6 +317,7 @@ std::ostream &operator<<(std::ostream &os, const MDSPerfMetricQuery &query);
 struct MDSPerfCollector : PerfCollector {
   std::map<MDSPerfMetricKey, PerformanceCounters> counters;
   std::set<mds_rank_t> delayed_ranks;
+  utime_t last_updated;
 
   MDSPerfCollector(MetricQueryID query_id)
     : PerfCollector(query_id) {

scantist-ossops-m2/ceph-ci

(cherry picked from commit c2470f271cce4d512f2cf00552c9b753e4c69f71)
Conflicts:
       src/pybind/mgr/stats/fs/perf_stats.py
(cherry picked from commit 3c5fe2d1e3985775c7cb4697658169831765f7dc)
       Resolved cherry-pick conflicts
Resolves: rhbz#2124417

src/mgr/ActivePyModules.cc
@@ -1407,6 +1407,11 @@ void ActivePyModules::remove_mds_perf_query(MetricQueryID query_id)
   }
 }
 
+void ActivePyModules::reregister_mds_perf_queries()
+{
+  server.reregister_mds_perf_queries();
+}
+
 PyObject *ActivePyModules::get_mds_perf_counters(MetricQueryID query_id)
 {
   MDSPerfCollector collector(query_id);
@@ -1449,6 +1454,11 @@ PyObject *ActivePyModules::get_mds_perf_counters(MetricQueryID query_id)
     f.close_section(); // i
   }
   f.close_section(); // counters
+
+  f.open_array_section("last_updated");
+  f.dump_float("last_updated_mono", collector.last_updated_mono);
+  f.close_section(); // last_updated
+
   f.close_section(); // metrics
 
   return f.get();

src/mgr/MDSPerfMetricTypes.h
@@ -317,6 +317,7 @@ std::ostream &operator<<(std::ostream &os, const MDSPerfMetricQuery &query);
 struct MDSPerfCollector : PerfCollector {
   std::map<MDSPerfMetricKey, PerformanceCounters> counters;
   std::set<mds_rank_t> delayed_ranks;
+  utime_t last_updated_mono;
 
   MDSPerfCollector(MetricQueryID query_id)
     : PerfCollector(query_id) {