ceph/ceph

rgw: `SimpleRadosReadCR` uses an async RADOS call
Don't go through the 'system object' cache. This also saves us the use
of the RADOS async completion processor.

Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

src/rgw/rgw_sync_module_aws.cc
@@ -1452,8 +1452,8 @@ public:
 
   int operate(const DoutPrefixProvider *dpp) override {
     reenter(this) {
-      yield call(new RGWSimpleRadosReadCR<rgw_sync_aws_multipart_upload_info>(dpp, sync_env->async_rados, sync_env->svc->sysobj,
-                                                                 status_obj, &status, false));
+      yield call(new RGWSimpleRadosReadCR<rgw_sync_aws_multipart_upload_info>(
+		   dpp, sync_env->store, status_obj, &status, false));
 
       if (retcode < 0 && retcode != -ENOENT) {
         ldpp_dout(dpp, 0) << "ERROR: failed to read sync status of object " << src_obj << " retcode=" << retcode << dendl;

scantist-ossops-m2/ceph-ci

src/rgw/driver/rados/rgw_sync_module_aws.cc
@@ -1452,8 +1452,8 @@ public:
 
   int operate(const DoutPrefixProvider *dpp) override {
     reenter(this) {
-      yield call(new RGWSimpleRadosReadCR<rgw_sync_aws_multipart_upload_info>(dpp, sync_env->async_rados, sync_env->svc->sysobj,
-                                                                 status_obj, &status, false));
+      yield call(new RGWSimpleRadosReadCR<rgw_sync_aws_multipart_upload_info>(
+		   dpp, sync_env->driver, status_obj, &status, false));
 
       if (retcode < 0 && retcode != -ENOENT) {
         ldpp_dout(dpp, 0) << "ERROR: failed to read sync status of object " << src_obj << " retcode=" << retcode << dendl;