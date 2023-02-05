86cf16f24769b5393e586c78fa1eda9234159979
Matan Breizman <mbreizma@redhat.com>
2023-02-05 14:53:11 +0000

    qa/*/crimson: Seperate Crimson's rbd api testing
    Signed-off-by: Matan Breizman <mbreizma@redhat.com>

---
+++ qa/workunits/rbd/crimson/test_crimson_librbd.sh
@@ -0,0 +1,36 @@
+#!/bin/sh -e
+
+if [ -n "${VALGRIND}" ]; then
+  valgrind ${VALGRIND} --suppressions=${TESTDIR}/valgrind.supp \
+    --error-exitcode=1 ceph_test_librbd
+else
+  # Run test cases indivually to allow better selection
+  # of ongoing Crimson development.
+  ceph_test_librbd --gtest_filter='TestLibRBD.*'
+  ceph_test_librbd --gtest_filter='EncryptedFlattenTest/0.*'
+  ceph_test_librbd --gtest_filter='EncryptedFlattenTest/1.*'
+  ceph_test_librbd --gtest_filter='EncryptedFlattenTest/2.*'
+  ceph_test_librbd --gtest_filter='EncryptedFlattenTest/3.*'
+  ceph_test_librbd --gtest_filter='EncryptedFlattenTest/4.*'
+  ceph_test_librbd --gtest_filter='EncryptedFlattenTest/5.*'
+  ceph_test_librbd --gtest_filter='EncryptedFlattenTest/6.*'
+  ceph_test_librbd --gtest_filter='EncryptedFlattenTest/7.*'
+  # <tracker>
+  # ceph_test_librbd --gtest_filter='DiffIterateTest/0.*'
+  # ceph_test_librbd --gtest_filter='DiffIterateTest/1.*'
+  ceph_test_librbd --gtest_filter='TestImageWatcher.*'
+  ceph_test_librbd --gtest_filter='TestInternal.*'
+  ceph_test_librbd --gtest_filter='TestMirroring.*'
+  # <tracker>
+  # ceph_test_librbd --gtest_filter='TestDeepCopy.*'
+  ceph_test_librbd --gtest_filter='TestGroup.*'
+  # <tracker>
+  # ceph_test_librbd --gtest_filter='TestMigration.*'
+  ceph_test_librbd --gtest_filter='TestMirroringWatcher.*'
+  ceph_test_librbd --gtest_filter='TestObjectMap.*'
+  ceph_test_librbd --gtest_filter='TestOperations.*'
+  ceph_test_librbd --gtest_filter='TestTrash.*'
+  ceph_test_librbd --gtest_filter='TestJournalEntries.*'
+  ceph_test_librbd --gtest_filter='TestJournalReplay.*'
+fi
+exit 0
