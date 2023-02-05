Matan-B/ceph

crimson/osd: make the ObjectContextRegistry per-PG
This patch moves the OBC registry from ShardServices (which
is basicaly a gateway to a bunch of PGs) into PG itself.
Dividing OBCs by PG (they truly belong to) minimizes the space
to search when e.g. checking whether a client is blocked or
not. Therefore, this commit is enabler of more performant
PR #47637.

The changeset draws the assumption that OBC registry and all
its users live on the same CPU core. It looks valid to me.

Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

src/crimson/osd/object_context_loader.h
@@ -3,7 +3,6 @@
 #include <seastar/core/future.hh>
 #include "crimson/common/errorator.h"
 #include "crimson/osd/object_context.h"
-#include "crimson/osd/shard_services.h"
 #include "crimson/osd/pg_backend.h"
 
 namespace crimson::osd {
@@ -14,9 +13,9 @@ public:
     ObjectContext::obc_accessing_option_t>;
 
   ObjectContextLoader(
-    ShardServices& _shard_services,
+    ObjectContextRegistry& _obc_services,
     PGBackend* _backend)
-    : shard_services{_shard_services},
+    : obc_registry{_obc_services},
       backend{_backend}
     {}
 
@@ -65,7 +64,7 @@ public:
   void notify_on_change(bool is_primary);
 
 private:
-  ShardServices &shard_services;
+  ObjectContextRegistry& obc_registry;
   PGBackend* backend;
   obc_accessing_list_t obc_set_accessing;
 };

scantist-ossops-m2/ceph-ci

src/crimson/osd/object_context_loader.h
@@ -3,7 +3,6 @@
 #include <seastar/core/future.hh>
 #include "crimson/common/errorator.h"
 #include "crimson/osd/object_context.h"
-#include "crimson/osd/shard_services.h"
 #include "crimson/osd/pg_backend.h"
 
 namespace crimson::osd {
@@ -14,10 +13,10 @@ public:
     ObjectContext::obc_accessing_option_t>;
 
   ObjectContextLoader(
-    ShardServices& _shard_services,
+    ObjectContextRegistry& _obc_registry,
     PGBackend& _backend,
     DoutPrefixProvider& dpp)
-    : shard_services{_shard_services},
+    : obc_registry{_obc_registry},
       backend{_backend},
       dpp{dpp}
     {}
@@ -67,7 +66,7 @@ public:
   void notify_on_change(bool is_primary);
 
 private:
-  ShardServices &shard_services;
+  ObjectContextRegistry& obc_registry;
   PGBackend& backend;
   DoutPrefixProvider& dpp;
   obc_accessing_list_t obc_set_accessing;