ceph/ceph

crimson/osd: snap trimming informs OpsExecuter about altered objects
Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

src/crimson/osd/ops_executer.h
@@ -42,6 +42,8 @@ class PG;
 
 // OpsExecuter -- a class for executing ops targeting a certain object.
 class OpsExecuter : public seastar::enable_lw_shared_from_this<OpsExecuter> {
+  friend class SnapTrimObjSubRequest;
+
   using call_errorator = crimson::errorator<
     crimson::stateful_ec,
     crimson::ct_error::enoent,

scantist-ossops-m2/ceph-ci

src/crimson/osd/ops_executer.h
@@ -42,6 +42,8 @@ class PG;
 
 // OpsExecuter -- a class for executing ops targeting a certain object.
 class OpsExecuter : public seastar::enable_lw_shared_from_this<OpsExecuter> {
+  friend class SnapTrimObjSubEvent;
+
   using call_errorator = crimson::errorator<
     crimson::stateful_ec,
     crimson::ct_error::enoent,