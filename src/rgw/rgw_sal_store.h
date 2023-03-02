e90dce6896674fcc5ae8d4628a8ad08441ac4e4a
Daniel Gryniewicz <dang@redhat.com>
2023-03-02 14:14:32 -0500

    RGW Zipper - Remove attrs from StoreObject
    Attributes are stored in state.attrset, not in a seperate attrs member.
    
    Signed-off-by: Daniel Gryniewicz <dang@redhat.com>

--- src/rgw/rgw_sal_store.h
+++ src/rgw/rgw_sal_store.h
@@ -170,7 +170,6 @@ class StoreObject : public Object {
   protected:
     RGWObjState state;
     Bucket* bucket;
-    Attrs attrs;
     bool delete_marker{false};
 
   public:
@@ -185,18 +184,15 @@ class StoreObject : public Object {
 
     StoreObject()
       : state(),
-      bucket(nullptr),
-      attrs()
+      bucket(nullptr)
       {}
     StoreObject(const rgw_obj_key& _k)
       : state(),
-      bucket(),
-      attrs()
+      bucket()
       { state.obj.key = _k; }
     StoreObject(const rgw_obj_key& _k, Bucket* _b)
       : state(),
-      bucket(_b),
-      attrs()
+      bucket(_b)
       { state.obj.init(_b->get_key(), _k); }
     StoreObject(StoreObject& _o) = default;
 
