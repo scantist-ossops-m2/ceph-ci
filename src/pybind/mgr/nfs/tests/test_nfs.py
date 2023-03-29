26b797d88cde3db217e95b4eb08b14a2ef8b923e
Dhairya Parmar <dparmar@redhat.com>
2023-03-29 23:28:59 +0530

    mgr/nfs/tests: mock check_cephfs_path
    Signed-off-by: Dhairya Parmar <dparmar@redhat.com>

--- src/pybind/mgr/nfs/tests/test_nfs.py
+++ src/pybind/mgr/nfs/tests/test_nfs.py
@@ -251,7 +251,9 @@ EXPORT {
                 mock.patch('nfs.export.check_fs', return_value=True), \
                 mock.patch('nfs.ganesha_conf.check_fs', return_value=True), \
                 mock.patch('nfs.export.ExportMgr._create_user_key',
-                           return_value='thekeyforclientabc'):
+                           return_value='thekeyforclientabc'), \
+                mock.patch('nfs.export.check_cephfs_path',
+                           return_value=(0, "cephfs path is valid")):
 
             rados.open_ioctx.return_value.__enter__.return_value = self.io_mock
             rados.open_ioctx.return_value.__exit__ = mock.Mock(return_value=None)
