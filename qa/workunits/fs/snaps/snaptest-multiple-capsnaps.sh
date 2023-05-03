4e069adbc089ba736863fe7650a91dd3700c4e0b
Milind Changire <mchangir@redhat.com>
2023-05-03 15:27:36 +0530

    qa: dump md5sum of content before checks
    Signed-off-by: Milind Changire <mchangir@redhat.com>

--- qa/workunits/fs/snaps/snaptest-multiple-capsnaps.sh
+++ qa/workunits/fs/snaps/snaptest-multiple-capsnaps.sh
@@ -15,21 +15,26 @@ mkdir .snap/5
 
 ls -al .snap/?/a
 
+echo "expecting asdf: head:$(md5sum a) - .snap/1/a:$(md5sum .snap/1/a)"
 grep asdf .snap/1/a
 stat .snap/1/a | grep 'Size: 5'
 
+echo "expecting asdf: head:$(md5sum a) - .snap/2/a:$(md5sum .snap/2/a)"
 grep asdf .snap/2/a
 stat .snap/2/a | grep 'Size: 5'
 stat .snap/2/a | grep -- '-rwxrwxrwx'
 
+echo "expecting qwer: head:$(md5sum a) - .snap/3/a:$(md5sum .snap/3/a)"
 grep qwer .snap/3/a
 stat .snap/3/a | grep 'Size: 5'
 stat .snap/3/a | grep -- '-rwxrwxrwx'
 
+echo "expecting qwer: head:$(md5sum a) - .snap/4/a:$(md5sum .snap/4/a)"
 grep qwer .snap/4/a
 stat .snap/4/a | grep 'Size: 5'
 stat .snap/4/a | grep -- '-rw-rw-rw-'
 
+echo "expecting zxcv: head:$(md5sum a) - .snap/5/a:$(md5sum .snap/5/a)"
 grep zxcv .snap/5/a
 stat .snap/5/a | grep 'Size: 5'
 stat .snap/5/a | grep -- '-rw-rw-rw-'
