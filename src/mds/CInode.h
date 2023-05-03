ceph/ceph

mds: fix possible ref leakage bug
Just in case any of the replica MDSs is down and won't get a chance
to reply us.

Fixes: https://tracker.ceph.com/issues/56011
Signed-off-by: Xiubo Li <xiubli@redhat.com>

src/mds/CInode.h
@@ -1118,7 +1118,7 @@ class CInode : public MDSCacheObject, public InodeStoreBase, public Counter<CIno
   // client caps
   client_t loner_cap = -1, want_loner_cap = -1;
 
-  uint32_t snap_update_ref = 0;
+  std::unordered_set<mds_rank_t> snap_update_ref_set;
 
 protected:
   ceph_lock_state_t *get_fcntl_lock_state() {

scantist-ossops-m2/ceph-ci

(cherry picked from commit 6ffec20c3b88022da11b4cd50de2de8961a1c18e)

src/mds/CInode.h
@@ -1118,7 +1118,7 @@ class CInode : public MDSCacheObject, public InodeStoreBase, public Counter<CIno
   // client caps
   client_t loner_cap = -1, want_loner_cap = -1;
 
-  uint32_t snap_update_ref = 0;
+  std::set<mds_rank_t> snap_update_ref_set;
 
 protected:
   ceph_lock_state_t *get_fcntl_lock_state() {