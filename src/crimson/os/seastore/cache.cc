xxhdx1985126/ceph

crimson/os/seastore/cached_extent: add on_invalidated interface
Signed-off-by: Xuehan Xu <xxhdx1985126@gmail.com>

src/crimson/os/seastore/cache.cc
@@ -824,6 +824,7 @@ void Cache::invalidate_extent(
     }
   }
   extent.state = CachedExtent::extent_state_t::INVALID;
+  extent.on_invalidated(t);
 }
 
 void Cache::mark_transaction_conflicted(

src/crimson/os/seastore/transaction.h
@@ -119,12 +119,15 @@ public:
 	ref->is_exist_mutation_pending()) {
       existing_block_stats.dec(ref);
       ref->state = CachedExtent::extent_state_t::INVALID;
+      ref->on_invalidated(*this);
       write_set.erase(*ref);
     } else if (ref->is_initial_pending()) {
       ref->state = CachedExtent::extent_state_t::INVALID;
+      ref->on_invalidated(*this);
       write_set.erase(*ref);
     } else if (ref->is_mutation_pending()) {
       ref->state = CachedExtent::extent_state_t::INVALID;
+      ref->on_invalidated(*this);
       write_set.erase(*ref);
       assert(ref->prior_instance);
       retired_set.insert(ref->prior_instance);
@@ -343,6 +346,7 @@ public:
   void invalidate_clear_write_set() {
     for (auto &&i: write_set) {
       i.state = CachedExtent::extent_state_t::INVALID;
+      i.on_invalidated(*this);
     }
     write_set.clear();
   }

scantist-ossops-m2/ceph-ci

src/crimson/os/seastore/cache.cc
@@ -812,7 +812,7 @@ void Cache::invalidate_extent(
 {
   if (!extent.may_conflict()) {
     assert(extent.transactions.empty());
-    extent.state = CachedExtent::extent_state_t::INVALID;
+    extent.set_invalid(t);
     return;
   }
 
@@ -829,7 +829,7 @@ void Cache::invalidate_extent(
       mark_transaction_conflicted(*i.t, extent);
     }
   }
-  extent.state = CachedExtent::extent_state_t::INVALID;
+  extent.set_invalid(t);
 }
 
 void Cache::mark_transaction_conflicted(

src/crimson/os/seastore/transaction.h
@@ -117,13 +117,13 @@ public:
     if (ref->is_exist_clean() ||
 	ref->is_exist_mutation_pending()) {
       existing_block_stats.dec(ref);
-      ref->state = CachedExtent::extent_state_t::INVALID;
+      ref->set_invalid(*this);
       write_set.erase(*ref);
     } else if (ref->is_initial_pending()) {
-      ref->state = CachedExtent::extent_state_t::INVALID;
+      ref->set_invalid(*this);
       write_set.erase(*ref);
     } else if (ref->is_mutation_pending()) {
-      ref->state = CachedExtent::extent_state_t::INVALID;
+      ref->set_invalid(*this);
       write_set.erase(*ref);
       assert(ref->prior_instance);
       retired_set.insert(ref->prior_instance);
@@ -348,7 +348,7 @@ public:
 
   void invalidate_clear_write_set() {
     for (auto &&i: write_set) {
-      i.state = CachedExtent::extent_state_t::INVALID;
+      i.set_invalid(*this);
     }
     write_set.clear();
   }