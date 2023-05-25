242add5a2d3a474cabc51ea19f594ef0dacf6785
Christopher Hoffman <choffman@redhat.com>
2023-05-25 16:48:12 +0000

    qa: Add tests to validate synced images on rbd-mirror
    Introduce integration tests to validate the checksums of images synced
    using snapshot based rbd mirroring.
    
    Run workload on an image using a krbd or nbd client. Take mirror
    snapshots of the image under workload. Unmap and demote the primary
    image. Calculate the MD5 checksum of the image's most recent mirror
    snapshot. Wait for the syncing of the mirror snapshot to complete.
    Promote the non-primary image in the other cluster. Calculate the
    MD5 checksum of the newly promoted image. Check whether the hashes of
    the demoted image's and promoted image's latest mirror snapshots are
    the same.
    
    The above test is run as part of two different workunits:
     - a workunit that validates the syncing of multiple images with
       workloads running on them
     - another workunit that validates the syncing of a single image with
       workload running on it and the primary image alternates between
       the clusters.
    
    Fixes: https://tracker.ceph.com/issues/61617
    Signed-off-by: Christopher Hoffman <choffman@redhat.com>
    Co-authored-by: Ramana Raja <rraja@redhat.com>

---
+++ qa/workunits/rbd/compare_mirror_images.sh
@@ -0,0 +1,158 @@
+#!/bin/bash
+
+set -ex
+
+IMG_PREFIX=image-
+RBD_MIRROR_MODE=snapshot
+MNTPT_PREFIX=/mnt/test
+WORKLOAD_TIMEOUT=5m
+
+. $(dirname $0)/rbd_mirror_helpers.sh
+
+launch_manual_msnaps() {
+  local cluster=$1
+  local pool=$2
+  local image=$3
+
+  for i in {1..30}; do
+    mirror_image_snapshot $cluster $pool $image
+    sleep 3s;
+  done
+}
+
+run_bench() {
+  local mountpt=$1
+  local timeout=$2
+
+  KERNEL_TAR_URL="https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.14.280.tar.gz"
+  sudo wget $KERNEL_TAR_URL -O $mountpt/kernel.tar.gz
+  sudo timeout $timeout bash -c "tar xvfz $mountpt/kernel.tar.gz -C $mountpt \
+    | pv -L 1k --timer &> /dev/null" || true
+}
+
+wait_for_demote_snap () {
+  local cluster=$1
+  local pool=$2
+  local image=$3
+
+  for s in 1 2 4 8 8 8 8 8 8 8 8 16 16; do
+    RET=$(rbd --cluster $cluster snap ls --all $pool/$image --format=json \
+            | jq 'last' \
+            | jq 'select(.name | contains("non_primary"))' \
+            | jq 'select(.namespace.state == "demoted")' \
+            | jq 'select(.namespace.complete == true)')
+    if [ "$RET" != "" ]; then
+      echo demoted snapshot received, continuing
+      break
+    fi
+
+    echo waiting for demoted snapshot...
+    sleep $s
+  done
+}
+
+compare_images() {
+  local img=${IMG_PREFIX}$1
+  local mntpt=${MNTPT_PREFIX}$1
+
+  sudo umount ${mntpt}
+  sudo rbd --cluster ${CLUSTER1} device unmap -t ${RBD_DEVICE_TYPE} \
+      ${POOL}/${img}
+  demote_image ${CLUSTER1} ${POOL} ${img}
+
+  # demote primary image and calculate hash of its latest mirror snapshot
+  local bdev demote demote_id demote_name demote_md5
+
+  demote=$(rbd --cluster ${CLUSTER1} snap ls --all ${POOL}/${img} --format=json \
+             | jq 'last' \
+             | jq 'select(.name | contains("mirror.primary"))' \
+             | jq 'select(.namespace.state == "demoted")')
+  if [[ $RBD_DEVICE_TYPE == "nbd" ]]; then
+    demote_id=$(echo $demote | jq -r '.id')
+    bdev=$(sudo rbd --cluster ${CLUSTER1} device map -t ${RBD_DEVICE_TYPE} \
+             --snap-id ${demote_id} ${POOL}/${img})
+  elif [[ $RBD_DEVICE_TYPE == "krbd" ]]; then
+    demote_name=$(echo $demote | jq -r '.name')
+    bdev=$(sudo rbd --cluster ${CLUSTER1} device map -t ${RBD_DEVICE_TYPE} \
+             ${POOL}/${img}@${demote_name})
+  else
+     echo "Unknown RBD_DEVICE_TYPE: ${RBD_DEVICE_TYPE}"
+     return 1
+  fi
+  demote_md5=$(sudo dd if=${bdev} bs=4M | md5sum | awk '{print $1}')
+  sudo rbd --cluster ${CLUSTER1} device unmap -t ${RBD_DEVICE_TYPE} ${bdev}
+
+  wait_for_demote_snap ${CLUSTER2} ${POOL} ${img}
+
+  promote_image ${CLUSTER2} ${POOL} ${img}
+
+  # promote non-primary image and calculate hash of its latest mirror snapshot
+  local promote promote_id promote_name promote_md5
+
+  promote=$(rbd --cluster ${CLUSTER2} snap ls --all ${POOL}/${img} --format=json \
+              | jq 'last' \
+              | jq 'select(.name | contains("mirror.primary"))')
+  if [[ $RBD_DEVICE_TYPE == "nbd" ]]; then
+    promote_id=$(echo $promote | jq -r '.id')
+    bdev=$(sudo rbd --cluster ${CLUSTER2} device map -t ${RBD_DEVICE_TYPE} \
+             --snap-id ${promote_id} ${POOL}/${img})
+  elif [[ $RBD_DEVICE_TYPE == "krbd" ]]; then
+    promote_name=$(echo $promote | jq -r '.name')
+    bdev=$(sudo rbd --cluster ${CLUSTER2} device map -t ${RBD_DEVICE_TYPE} \
+             ${POOL}/${img}@${promote_name})
+  else
+     echo "Unknown RBD_DEVICE_TYPE: ${RBD_DEVICE_TYPE}"
+     return 1
+  fi
+  promote_md5=$(sudo dd if=${bdev} bs=4M | md5sum | awk '{print $1}')
+  sudo rbd --cluster ${CLUSTER2} device unmap -t ${RBD_DEVICE_TYPE} ${bdev}
+
+  if [ "${demote_md5}" != "${promote_md5}" ]; then
+          return 1
+  fi
+}
+
+setup
+
+start_mirrors ${CLUSTER1}
+start_mirrors ${CLUSTER2}
+
+for i in {1..10}; do
+  for j in {1..10}; do
+    IMG=${IMG_PREFIX}${j}
+    MNTPT=${MNTPT_PREFIX}${j}
+    create_image_and_enable_mirror ${CLUSTER1} ${POOL} ${IMG} \
+      ${RBD_MIRROR_MODE} 10G
+    BDEV=$(sudo rbd --cluster ${CLUSTER1} device map -t ${RBD_DEVICE_TYPE} \
+             ${POOL}/${IMG})
+    sudo mkfs.ext4 ${BDEV}
+    sudo mkdir -p ${MNTPT}
+    sudo mount ${BDEV} ${MNTPT}
+    # create mirror snapshots under I/O
+    launch_manual_msnaps ${CLUSTER1} ${POOL} ${IMG} &
+    run_bench ${MNTPT} ${WORKLOAD_TIMEOUT} &
+  done
+  wait
+
+  pids=''
+  for j in {1..10}; do
+    compare_images $j &
+    pids+=" $!"
+  done
+
+  for pid in $pids; do
+    wait "$pid"
+    RC=$?
+    echo $pid $RC
+    if [ $RC != 0 ]; then
+      exit $RC
+    fi
+  done
+
+  for j in {1..10}; do
+    IMG=${IMG_PREFIX}${j}
+    remove_image ${CLUSTER2} ${POOL} ${IMG}
+  done
+done
+
+echo OK
