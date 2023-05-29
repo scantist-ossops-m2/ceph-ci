1c2181838e9c446fe64a1cd833929198fbbbb3f4
Ali Masarwa <ali.saed.masarwa@gmail.com>
2023-05-29 17:40:15 +0300

    RGW:notifications: persistent topics are not deleted via radosgw-admin
    Signed-off-by: Ali Masarwa <ali.saed.masarwa@gmail.com>

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -10534,6 +10534,12 @@ next:
       return EINVAL;
     }
 
+    ret = rgw::notify::remove_persistent_topic(dpp(), static_cast<rgw::sal::RadosStore*>(store)->getRados()->get_notif_pool_ctx(), topic_name, null_yield);
+    if (ret < 0) {
+      cerr << "ERROR: could not remove persistent topic: " << cpp_strerror(-ret) << std::endl;
+      return -ret;
+    }
+
     RGWPubSub ps(store, tenant);
 
     ret = ps.remove_topic(dpp(), topic_name, null_yield);
