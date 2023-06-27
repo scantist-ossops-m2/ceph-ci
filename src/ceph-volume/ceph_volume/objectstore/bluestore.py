056aec0654d785a25780ce24095673f54d5cb88c
Guillaume Abrioux <gabrioux@ibm.com>
2023-06-27 15:39:46 +0200

    ceph-volume: add type annotation in baseobjectstore.py
    This adds the type annotation in baseobjectstore.py
    This also addresses some flake8 issues.
    
    Signed-off-by: Guillaume Abrioux <gabrioux@ibm.com>

--- src/ceph-volume/ceph_volume/objectstore/bluestore.py
+++ src/ceph-volume/ceph_volume/objectstore/bluestore.py
@@ -1,23 +1,27 @@
 import logging
 from .baseobjectstore import BaseObjectStore
 from ceph_volume.util import system
+from typing import Optional, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    import argparse
 
 logger = logging.getLogger(__name__)
 
 
 class BlueStore(BaseObjectStore):
-    def __init__(self, args):
+    def __init__(self, args: "argparse.Namespace") -> None:
         super().__init__(args)
-        self.args = args
+        self.args: "argparse.Namespace" = args
         self.objectstore = 'bluestore'
-        self.osd_id = None
-        self.osd_fsid = ''
-        self.key = None
-        self.block_device_path = ''
-        self.wal_device_path = ''
-        self.db_device_path = ''
+        self.osd_id: str = ''
+        self.osd_fsid: str = ''
+        self.key: Optional[str] = None
+        self.block_device_path: str = ''
+        self.wal_device_path: str = ''
+        self.db_device_path: str = ''
 
-    def add_objectstore_opts(self):
+    def add_objectstore_opts(self) -> None:
         """
         Create the files for the OSD to function. A normal call will look like:
 
@@ -28,8 +32,8 @@ class BlueStore(BaseObjectStore):
                     --keyring /var/lib/ceph/osd/ceph-0/keyring \
                     --setuser ceph --setgroup ceph
 
-        In some cases it is required to use the keyring, when it is passed in as
-        a keyword argument it is used as part of the ceph-osd command
+        In some cases it is required to use the keyring, when it is passed
+        in as a keyword argument it is used as part of the ceph-osd command
         """
 
         if self.wal_device_path:
@@ -45,11 +49,14 @@ class BlueStore(BaseObjectStore):
             system.chown(self.db_device_path)
 
         if self.get_osdspec_affinity():
-            self.osd_mkfs_cmd.extend(['--osdspec-affinity', self.get_osdspec_affinity()])
+            self.osd_mkfs_cmd.extend(['--osdspec-affinity',
+                                      self.get_osdspec_affinity()])
 
         """
         When running in containers the --mkfs on raw device sometimes fails
-        to acquire a lock through flock() on the device because systemd-udevd holds one temporarily.
-        See KernelDevice.cc and _lock() to understand how ceph-osd acquires the lock.
-        Because this is really transient, we retry up to 5 times and wait for 1 sec in-between
+        to acquire a lock through flock() on the device because systemd-udevd
+        holds one temporarily. See KernelDevice.cc and _lock() to understand
+        how ceph-osd acquires the lock.
+        Because this is really transient, we retry up to 5 times and wait for
+        1 sec in-between
         """
