84764545a6d9212c39000e928c6a770b897fc431
Pere Diaz Bou <pere-altea@hotmail.com>
2023-06-27 13:44:05 +0200

    mgr/dashboard: adapt test_hosts
    Signed-off-by: Pere Diaz Bou <pere-altea@hotmail.com>

--- src/pybind/mgr/dashboard/controllers/host.py
+++ src/pybind/mgr/dashboard/controllers/host.py
@@ -9,11 +9,12 @@ from typing import Dict, List, Optional
 import cherrypy
 from mgr_util import merge_dicts
 from orchestrator import HostSpec
-from ..plugins.ttl_cache import ttl_cache
 
 from .. import mgr
 from ..exceptions import DashboardException
+from ..plugins.ttl_cache import ttl_cache
 from ..security import Scope
+from ..services._paginate import ListPaginator
 from ..services.ceph_service import CephService
 from ..services.exception import handle_orchestrator_error
 from ..services.orchestrator import OrchClient, OrchFeature
@@ -22,7 +23,6 @@ from . import APIDoc, APIRouter, BaseController, Endpoint, EndpointDoc, \
     ReadPermission, RESTController, Task, UIRouter, UpdatePermission, \
     allow_empty_body
 from ._version import APIVersion
-from ..services._paginate import ListPaginator
 from .orchestrator import raise_if_no_orchestrator
 
 LIST_HOST_SCHEMA = {
@@ -304,19 +304,23 @@ class Host(RESTController):
                      'facts': (bool, 'Host Facts')
                  },
                  responses={200: LIST_HOST_SCHEMA})
-    @RESTController.MethodMap(version=APIVersion(1, 2))
-    def list(self, sources=None, facts=False, offset: int = 0, 
+    @RESTController.MethodMap(version=APIVersion(1, 3))
+    def list(self, sources=None, facts=False, offset: int = 0,
              limit: int = 5, search: str = '', sort: str = ''):
         hosts = get_hosts(sources)
         params = ['hostname']
         paginator = ListPaginator(int(offset), int(limit), sort, search, hosts,
                                   searchable_params=params, sortable_params=params,
                                   default_sort='+hostname')
+        # pylint: disable=unnecessary-comprehension
         hosts = [host for host in paginator.list()]
         orch = OrchClient.instance()
         cherrypy.response.headers['X-Total-Count'] = paginator.get_count()
         for host in hosts:
-            host['service_instances'] = populate_service_instances(host['hostname'], host['services'])
+            if 'services' not in host:
+                host['services'] = []
+            host['service_instances'] = populate_service_instances(
+                host['hostname'], host['services'])
         if str_to_bool(facts):
             if orch.available():
                 if not orch.get_missing_features(['get_facts']):
@@ -442,12 +446,16 @@ class Host(RESTController):
         return [d.to_dict() for d in daemons]
 
     @handle_orchestrator_error('host')
+    @RESTController.MethodMap(version=APIVersion(1, 2))
     def get(self, hostname: str) -> Dict:
         """
         Get the specified host.
         :raises: cherrypy.HTTPError: If host not found.
         """
-        return get_host(hostname)
+        host = get_host(hostname)
+        host['service_instances'] = populate_service_instances(
+            host['hostname'], host['services'])
+        return host
 
     @raise_if_no_orchestrator([OrchFeature.HOST_LABEL_ADD,
                                OrchFeature.HOST_LABEL_REMOVE,
