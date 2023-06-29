37bf05edd3536859a5a544a197d943e9dfbdf7c8
Matan Breizman <mbreizma@redhat.com>
2023-06-29 12:11:23 +0000

    crimson/osd/osd.cc: handle_update_log_missing* don't decode payload.
    Payload is already decoded in IOHandler::read_message (decode_message).
    
    Signed-off-by: Matan Breizman <mbreizma@redhat.com>

--- src/crimson/osd/osd.cc
+++ src/crimson/osd/osd.cc
@@ -1137,7 +1137,6 @@ seastar::future<> OSD::handle_update_log_missing(
   crimson::net::ConnectionRef conn,
   Ref<MOSDPGUpdateLogMissing> m)
 {
-  m->decode_payload();
   return pg_shard_manager.start_pg_operation<LogMissingRequest>(
     std::move(conn),
     std::move(m)).second;
@@ -1147,7 +1146,6 @@ seastar::future<> OSD::handle_update_log_missing_reply(
   crimson::net::ConnectionRef conn,
   Ref<MOSDPGUpdateLogMissingReply> m)
 {
-  m->decode_payload();
   return pg_shard_manager.start_pg_operation<LogMissingRequestReply>(
     std::move(conn),
     std::move(m)).second;
