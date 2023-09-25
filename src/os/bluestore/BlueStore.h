abc132b8ea757e0aa292db126236a9a70f7d64fe
Pere Diaz Bou <pdiabou@redhat.com>
2023-09-25 12:11:12 +0200

    os/bluestore: add missing add_blobs
    Signed-off-by: Pere Diaz Bou <pdiabou@redhat.com>

--- src/os/bluestore/BlueStore.h
+++ src/os/bluestore/BlueStore.h
@@ -614,6 +614,7 @@ public:
       ceph_assert(!shared_blob);
       sbid_unloaded = sb->get_sbid();
       shared_blob = sb;
+      collection = sb->coll;
       ceph_assert(get_cache());
       get_cache()->add_blob();
     }
@@ -1638,6 +1639,7 @@ private:
     BlobRef new_blob() {
       BlobRef b = new Blob();
       b->collection = this;
+      b->get_cache()->add_blob();
       return b;
     }
 
