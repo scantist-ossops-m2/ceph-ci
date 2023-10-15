xxhdx1985126/ceph

crimson/os/seastore/lba_manager: decrease refcount of the physical lba mappings when removing indirect lba mappings
Signed-off-by: Xuehan Xu <xxhdx1985126@gmail.com>

src/crimson/os/seastore/lba_manager/btree/btree_lba_manager.cc
@@ -683,6 +683,79 @@ void BtreeLBAManager::register_metrics()
   );
 }
 
+BtreeLBAManager::ref_iertr::future<std::list<std::pair<paddr_t, extent_len_t>>>
+BtreeLBAManager::_decref_intermediate(
+  Transaction &t,
+  laddr_t addr,
+  extent_len_t len)
+{
+  auto c = get_context(t);
+  return with_btree<LBABtree>(
+    cache,
+    c,
+    [c, addr, len](auto &btree) mutable {
+    return btree.lower_bound(
+      c, addr
+    ).si_then([&btree, addr, len, c](auto iter) {
+      return seastar::do_with(
+	std::move(iter),
+	(extent_len_t)0,
+	std::list<std::pair<paddr_t, extent_len_t>>(),
+	[&btree, addr, len, c](auto &iter, auto &scanned, auto &removed) {
+	return trans_intr::repeat([&iter, &btree, &scanned,
+				  addr, len, c, &removed] {
+	  ceph_assert(!iter.is_end() && iter.get_key() == addr + scanned);
+	  auto val = iter.get_val();
+	  ceph_assert(val.refcount >= 1);
+	  val.refcount -= 1;
+	  LOG_PREFIX(BtreeLBAManager::_decref_intermediate);
+	  TRACET("decreased refcount of intermediate key {} -- {}",
+	    c.trans,
+	    iter.get_key(),
+	    val);
+
+	  auto after_decref = [val, &iter, &scanned, len, c]
+			      (auto it, bool step_forward) {
+	    scanned += val.len;
+	    if (scanned == len) {
+	      return base_iertr::make_ready_future<
+		seastar::stop_iteration>(
+		  seastar::stop_iteration::yes);
+	    } else {
+	      ceph_assert(scanned < len);
+	      ceph_assert(!it.is_end());
+	      if (step_forward) {
+		return it.next(c).si_then([&iter](auto it) {
+		  iter = std::move(it);
+		  return seastar::stop_iteration::no;
+		});
+	      } else {
+		iter = std::move(it);
+		return base_iertr::make_ready_future<
+		  seastar::stop_iteration>(seastar::stop_iteration::no);
+	      }
+	    }
+	  };
+	  if (!val.refcount) {
+	    removed.emplace_back(val.pladdr.get_paddr(), val.len);
+	    return btree.remove(c, iter
+	    ).si_then([f=std::move(after_decref)](auto it) {
+	      return f(std::move(it), false);
+	    });
+	  } else {
+	    return btree.update(c, iter, val, nullptr
+	    ).si_then([f=std::move(after_decref)](auto it) {
+	      return f(std::move(it), true);
+	    });
+	  }
+	}).si_then([&removed] {
+	  return std::move(removed);
+	});
+      });
+    });
+  });
+}
+
 BtreeLBAManager::update_refcount_ret
 BtreeLBAManager::update_refcount(
   Transaction &t,
@@ -701,15 +774,27 @@ BtreeLBAManager::update_refcount(
       return out;
     },
     nullptr
-  ).si_then([&t, addr, delta, FNAME](auto result) {
+  ).si_then([&t, addr, delta, FNAME, this](auto result) {
     auto ret_val = (result.index() == 0) ? std::get<0>(result)
 					 : std::get<1>(result)->get_map_val();
     DEBUGT("laddr={}, delta={} done -- {}", t, addr, delta, ret_val);
-    return ref_update_result_t{
-      ret_val.refcount,
-      ret_val.pladdr,
-      ret_val.len
-    };
+    auto fut = ref_iertr::make_ready_future<
+      std::list<std::pair<paddr_t, extent_len_t>>>();
+    if (!ret_val.refcount && ret_val.pladdr.is_laddr()) {
+      fut = _decref_intermediate(
+	t,
+	ret_val.pladdr.get_laddr(),
+	ret_val.len
+      );
+    }
+    return fut.si_then([ret_val](auto removed) {
+      return ref_update_result_t{
+	ret_val.refcount,
+	ret_val.pladdr,
+	ret_val.len,
+	std::move(removed)
+      };
+    });
   });
 }
 

src/crimson/os/seastore/lba_manager/btree/btree_lba_manager.h
@@ -301,6 +301,12 @@ private:
     paddr_t actual_addr,
     uint32_t refcnt,
     LogicalCachedExtent*);
+
+  ref_iertr::future<std::list<std::pair<paddr_t, extent_len_t>>>
+  _decref_intermediate(
+    Transaction &t,
+    laddr_t addr,
+    extent_len_t len);
 };
 using BtreeLBAManagerRef = std::unique_ptr<BtreeLBAManager>;
 

scantist-ossops-m2/ceph-ci

(cherry picked from commit 31204c2e1cb05ac1476abbcc5d5198f38e326095)

src/crimson/os/seastore/lba_manager/btree/btree_lba_manager.cc
@@ -616,6 +616,56 @@ void BtreeLBAManager::register_metrics()
   );
 }
 
+BtreeLBAManager::ref_iertr::future<std::optional<std::pair<paddr_t, extent_len_t>>>
+BtreeLBAManager::_decref_intermediate(
+  Transaction &t,
+  laddr_t addr,
+  extent_len_t len)
+{
+  auto c = get_context(t);
+  return with_btree<LBABtree>(
+    cache,
+    c,
+    [c, addr, len](auto &btree) mutable {
+    return btree.upper_bound_right(
+      c, addr
+    ).si_then([&btree, addr, len, c](auto iter) {
+      return seastar::do_with(
+	std::move(iter),
+	[&btree, addr, len, c](auto &iter) {
+	ceph_assert(!iter.is_end());
+	ceph_assert(iter.get_key() <= addr);
+	auto val = iter.get_val();
+	ceph_assert(iter.get_key() + val.len >= addr + len);
+	ceph_assert(val.pladdr.is_paddr());
+	ceph_assert(val.refcount >= 1);
+	val.refcount -= 1;
+
+	LOG_PREFIX(BtreeLBAManager::_decref_intermediate);
+	TRACET("decreased refcount of intermediate key {} -- {}",
+	  c.trans,
+	  iter.get_key(),
+	  val);
+
+	if (!val.refcount) {
+	  return btree.remove(c, iter
+	  ).si_then([val] {
+	    return std::make_optional<
+	      std::pair<paddr_t, extent_len_t>>(
+		val.pladdr.get_paddr(), val.len);
+	  });
+	} else {
+	  return btree.update(c, iter, val, nullptr
+	  ).si_then([](auto) {
+	    return seastar::make_ready_future<
+	      std::optional<std::pair<paddr_t, extent_len_t>>>(std::nullopt);
+	  });
+	}
+      });
+    });
+  });
+}
+
 BtreeLBAManager::update_refcount_ret
 BtreeLBAManager::update_refcount(
   Transaction &t,
@@ -634,13 +684,32 @@ BtreeLBAManager::update_refcount(
       return out;
     },
     nullptr
-  ).si_then([&t, addr, delta, FNAME](auto result) {
+  ).si_then([&t, addr, delta, FNAME, this](auto result) {
     DEBUGT("laddr={}, delta={} done -- {}", t, addr, delta, result);
-    return ref_update_result_t{
-      result.refcount,
-      result.pladdr,
-      result.len
-     };
+    auto fut = ref_iertr::make_ready_future<
+      std::optional<std::pair<paddr_t, extent_len_t>>>();
+    if (!result.refcount && result.pladdr.is_laddr()) {
+      fut = _decref_intermediate(
+	t,
+	result.pladdr.get_laddr(),
+	result.len
+      );
+    }
+    return fut.si_then([result](auto removed) {
+      if (result.pladdr.is_laddr()
+	  && removed) {
+	return ref_update_result_t{
+	  result.refcount,
+	  removed->first,
+	  removed->second};
+      } else {
+	return ref_update_result_t{
+	  result.refcount,
+	  result.pladdr,
+	  result.len
+	};
+      }
+    });
   });
 }
 

src/crimson/os/seastore/lba_manager/btree/btree_lba_manager.h
@@ -375,6 +375,11 @@ private:
     op_context_t<laddr_t> c,
     std::list<BtreeLBAMappingRef> &pin_list);
 
+  ref_iertr::future<std::optional<std::pair<paddr_t, extent_len_t>>>
+  _decref_intermediate(
+    Transaction &t,
+    laddr_t addr,
+    extent_len_t len);
 };
 using BtreeLBAManagerRef = std::unique_ptr<BtreeLBAManager>;
 