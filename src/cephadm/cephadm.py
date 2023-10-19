0639f32918daaa7696f1a16895b22b8236b9d622
John Mulligan <jmulligan@redhat.com>
2023-10-19 09:42:34 -0400

    cephadm: move extract_uid_gid func to container_types module
    While extract_uid_gid isn't a perfect fit for container_types it is a
    fairly fundamental function for working with containers in cephadm and
    doesn't require anything beyond types in containers_types and that
    module's existing imports.  Moving extract_uid_gid should allow us to
    more easily move other functions in the future.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- src/cephadm/cephadm.py
+++ src/cephadm/cephadm.py
@@ -154,6 +154,7 @@ from cephadmlib.container_types import (
     CephContainer,
     InitContainer,
     is_container_running,
+    extract_uid_gid,
 )
 from cephadmlib.decorators import (
     deprecated_command,
@@ -2838,37 +2839,6 @@ def get_container(
         ptrace=ptrace,
         host_network=host_network,
     )
-
-
-def extract_uid_gid(ctx, img='', file_path='/var/lib/ceph'):
-    # type: (CephadmContext, str, Union[str, List[str]]) -> Tuple[int, int]
-
-    if not img:
-        img = ctx.image
-
-    if isinstance(file_path, str):
-        paths = [file_path]
-    else:
-        paths = file_path
-
-    ex: Optional[Tuple[str, RuntimeError]] = None
-
-    for fp in paths:
-        try:
-            out = CephContainer(
-                ctx,
-                image=img,
-                entrypoint='stat',
-                args=['-c', '%u %g', fp]
-            ).run(verbosity=CallVerbosity.QUIET_UNLESS_ERROR)
-            uid, gid = out.split(' ')
-            return int(uid), int(gid)
-        except RuntimeError as e:
-            ex = (fp, e)
-    if ex:
-        raise Error(f'Failed to extract uid/gid for path {ex[0]}: {ex[1]}')
-
-    raise RuntimeError('uid/gid not found')
 
 
 def deploy_daemon(
