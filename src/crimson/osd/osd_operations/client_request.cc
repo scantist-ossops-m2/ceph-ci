ceph/ceph

crimson/osd: add third future to pg_rep_op_fut_t
change PG::do_osd_ops_execute() and PG::do_osd_ops() return type
of pg_rep_op_fut_t to include another future.
The added future will represent the error_log future which
is run as part of the pg pipeline 'process' stage (Exclusive).

Signed-off-by: Matan Breizman <mbreizma@redhat.com>

src/crimson/osd/osd_operations/client_request.cc
@@ -312,23 +312,28 @@ ClientRequest::do_process(
     }
   }
   return pg->do_osd_ops(m, conn, obc, op_info, snapc).safe_then_unpack_interruptible(
-    [this, pg, &ihref](auto submitted, auto all_completed) mutable {
-      return submitted.then_interruptible([this, pg, &ihref] {
-	return ihref.enter_stage<interruptor>(client_pp(*pg).wait_repop, *this);
+    [this, pg, &ihref](auto submitted_fut, auto error_log_fut, auto all_completed) mutable {
+      return submitted_fut.then_interruptible([this, pg, &ihref] {
+        return ihref.enter_stage<interruptor>(client_pp(*pg).wait_repop, *this);
       }).then_interruptible(
-	[this, pg, all_completed=std::move(all_completed), &ihref]() mutable {
-	  return all_completed.safe_then_interruptible(
-	    [this, pg, &ihref](MURef<MOSDOpReply> reply) {
-	      return ihref.enter_stage<interruptor>(client_pp(*pg).send_reply, *this
-	      ).then_interruptible(
-		[this, reply=std::move(reply)]() mutable {
-		  logger().debug("{}: sending response", *this);
-		  return conn->send(std::move(reply));
-		});
-	    }, crimson::ct_error::eagain::handle([this, pg, &ihref]() mutable {
-	      return process_op(ihref, pg);
-	    }));
-	});
+        [this, pg, error_log_fut=std::move(error_log_fut),
+         all_completed=std::move(all_completed), &ihref]() mutable {
+      return error_log_fut.then([this, pg, &ihref] {
+        return ihref.enter_stage<interruptor>(client_pp(*pg).wait_repop, *this);
+      }).then_interruptible([this, pg, all_completed=std::move(all_completed), &ihref]() mutable {
+      return all_completed.safe_then_interruptible(
+        [this, pg, &ihref](MURef<MOSDOpReply> reply) {
+          return ihref.enter_stage<interruptor>(client_pp(*pg).send_reply, *this
+          ).then_interruptible(
+        [this, reply=std::move(reply)]() mutable {
+          logger().debug("{}: sending response", *this);
+          return conn->send(std::move(reply));
+        });
+        }, crimson::ct_error::eagain::handle([this, pg, &ihref]() mutable {
+          return process_op(ihref, pg);
+        }));
+      });
+    });
     }, crimson::ct_error::eagain::handle([this, pg, &ihref]() mutable {
       return process_op(ihref, pg);
     }));

scantist-ossops-m2/ceph-ci

src/crimson/osd/osd_operations/client_request.cc
@@ -312,23 +312,25 @@ ClientRequest::do_process(
     }
   }
   return pg->do_osd_ops(m, conn, obc, op_info, snapc).safe_then_unpack_interruptible(
-    [this, pg, &ihref](auto submitted, auto all_completed) mutable {
-      return submitted.then_interruptible([this, pg, &ihref] {
-	return ihref.enter_stage<interruptor>(client_pp(*pg).wait_repop, *this);
+    [this, pg, &ihref](auto submitted, auto error_log, auto all_completed) mutable {
+      return submitted.then_interruptible([this, pg, &ihref, error_log = std::move(error_log)] () mutable {
+        return error_log.then([this, pg, &ihref] {
+          return ihref.enter_stage<interruptor>(client_pp(*pg).wait_repop, *this);
+        });
       }).then_interruptible(
-	[this, pg, all_completed=std::move(all_completed), &ihref]() mutable {
-	  return all_completed.safe_then_interruptible(
-	    [this, pg, &ihref](MURef<MOSDOpReply> reply) {
-	      return ihref.enter_stage<interruptor>(client_pp(*pg).send_reply, *this
-	      ).then_interruptible(
-		[this, reply=std::move(reply)]() mutable {
-		  logger().debug("{}: sending response", *this);
-		  return conn->send(std::move(reply));
-		});
-	    }, crimson::ct_error::eagain::handle([this, pg, &ihref]() mutable {
-	      return process_op(ihref, pg);
-	    }));
-	});
+        [this, pg, all_completed=std::move(all_completed), &ihref]() mutable {
+          return all_completed.safe_then_interruptible(
+            [this, pg, &ihref](MURef<MOSDOpReply> reply) {
+              return ihref.enter_stage<interruptor>(client_pp(*pg).send_reply, *this
+              ).then_interruptible(
+                [this, reply=std::move(reply)]() mutable {
+                  logger().debug("{}: sending response", *this);
+                  return conn->send(std::move(reply));
+                });
+            }, crimson::ct_error::eagain::handle([this, pg, &ihref]() mutable {
+              return process_op(ihref, pg);
+            }));
+        });
     }, crimson::ct_error::eagain::handle([this, pg, &ihref]() mutable {
       return process_op(ihref, pg);
     }));