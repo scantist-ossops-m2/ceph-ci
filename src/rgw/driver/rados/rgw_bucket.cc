yuvalif/ceph

rgw/multisite-notification: Add omap object to store the mapping between bucket and topics.
Signed-off-by: kchheda3 <kchheda3@bloomberg.net>

src/rgw/driver/rados/rgw_bucket.cc
@@ -2132,6 +2132,83 @@ int RGWMetadataHandlerPut_Bucket::put_post(const DoutPrefixProvider *dpp)
   return ret;
 }
 
+int update_bucket_topic_mappings(const DoutPrefixProvider* dpp,
+                                 RGWBucketCompleteInfo* orig_bci,
+                                 RGWBucketCompleteInfo* current_bci,
+                                 rgw::sal::Driver* driver) {
+  const auto decode_attrs =
+      [&](const rgw::sal::Attrs& attrs,
+          rgw_pubsub_bucket_topics& bucket_topics) -> int {
+    auto iter = attrs.find(RGW_ATTR_BUCKET_NOTIFICATION);
+    if (iter == attrs.end()) {
+      return 0;
+    }
+    try {
+      const auto& bl = iter->second;
+      auto biter = bl.cbegin();
+      bucket_topics.decode(biter);
+    } catch (buffer::error& err) {
+      return -EIO;
+    }
+    return 0;
+  };
+  rgw_pubsub_bucket_topics old_bucket_topics;
+  if (orig_bci) {
+    auto ret = decode_attrs(orig_bci->attrs, old_bucket_topics);
+    if (ret < 0) {
+      ldpp_dout(dpp, 1)
+          << "ERROR: failed to decode OLD bucket topics for bucket: "
+          << orig_bci->info.bucket.name << dendl;
+      return ret;
+    }
+  }
+  rgw_pubsub_bucket_topics current_bucket_topics;
+  auto ret = decode_attrs(current_bci->attrs, current_bucket_topics);
+  if (ret < 0) {
+    ldpp_dout(dpp, 1)
+        << "ERROR: failed to decode current bucket topics for bucket: "
+        << current_bci->info.bucket.name << dendl;
+    return ret;
+  }
+  // fetch the list of subscribed topics stored inside old_bucket attrs.
+  std::unordered_map<std::string, rgw_pubsub_topic> old_topics;
+  for (const auto& [_, topic_filter] : old_bucket_topics.topics) {
+    old_topics[topic_filter.topic.name] = topic_filter.topic;
+  }
+  // fetch the list of subscribed topics stored inside current_bucket attrs.
+  std::unordered_map<std::string, rgw_pubsub_topic> current_topics;
+  for (const auto& [_, topic_filter] : current_bucket_topics.topics) {
+    current_topics[topic_filter.topic.name] = topic_filter.topic;
+  }
+  auto bucket = driver->get_bucket(current_bci->info);
+  // traverse thru old topics and check if they are not in current, then delete
+  // the mapping, if present in both current and old then delete from current
+  // set as we do not need to update those mapping.
+  for (const auto& [topic_name, topic] : old_topics) {
+    auto it = current_topics.find(topic_name);
+    if (it == current_topics.end()) {
+      const auto op_ret = driver->update_bucket_topic_mapping(
+          topic, bucket.get(), /*add_mapping=*/false, null_yield, dpp);
+      if (op_ret < 0) {
+        ret = op_ret;
+      }
+    } else {
+      // already that attr is present, so do not update the mapping.
+      current_topics.erase(it);
+    }
+  }
+  // traverse thru current topics and check if they are any present, then add
+  // the mapping.
+  for (const auto& [topic_name, topic] : current_topics) {
+    const auto op_ret = driver->update_bucket_topic_mapping(
+        topic, bucket.get(), /*add_mapping=*/true, null_yield, dpp);
+    if (op_ret < 0) {
+      ret = op_ret;
+    }
+  }
+  return ret;
+}
+
 static void get_md5_digest(const RGWBucketEntryPoint *be, string& md5_digest) {
 
    char md5[CEPH_CRYPTO_MD5_DIGESTSIZE * 2 + 1];
@@ -2648,6 +2725,21 @@ int RGWMetadataHandlerPut_BucketInstance::put_post(const DoutPrefixProvider *dpp
     }
   } /* update lc */
 
+  /* update bucket topic mapping */
+  {
+    auto* orig_obj = dynamic_cast<RGWBucketInstanceMetadataObject*>(old_obj);
+    auto* orig_bci = (orig_obj ? &orig_obj->get_bci() : nullptr);
+    ret = update_bucket_topic_mappings(dpp, orig_bci, &bci, bihandler->driver);
+    if (ret < 0) {
+      ldpp_dout(dpp, 0) << __func__
+                        << " failed to apply bucket topic mapping for "
+                        << bci.info.bucket.name << dendl;
+      return ret;
+    }
+    ldpp_dout(dpp, 20) << __func__
+                       << " successfully applied bucket topic mapping for "
+                       << bci.info.bucket.name << dendl;
+  }
   return STATUS_APPLIED;
 }
 

src/rgw/driver/rados/rgw_sal_rados.cc
@@ -1170,6 +1170,91 @@ int RadosStore::remove_topic_v2(const std::string& topic_name,
                                            params, objv_tracker, y, dpp);
 }
 
+int RadosStore::update_bucket_topic_mapping(const rgw_pubsub_topic& topic,
+                                            rgw::sal::Bucket* bucket,
+                                            bool add_mapping,
+                                            optional_yield y,
+                                            const DoutPrefixProvider* dpp) {
+  const auto& bucket_str =
+      rgw_make_bucket_entry_name(bucket->get_tenant(), bucket->get_name());
+  bufferlist empty_bl;
+  librados::ObjectWriteOperation op;
+  int ret = 0;
+  if (add_mapping) {
+    std::map<std::string, bufferlist> mapping{{bucket_str, empty_bl}};
+    op.omap_set(mapping);
+  } else {
+    std::set<std::string> to_rm{{bucket_str}};
+    op.omap_rm_keys(to_rm);
+  }
+  ret = rgw_rados_operate(dpp, rados->get_notif_pool_ctx(),
+                          get_bucket_topic_mapping_oid(topic), &op, y);
+  const auto str = add_mapping ? "add" : "remove";
+  if (ret < 0) {
+    ldpp_dout(dpp, 1) << "ERROR: failed to " << str
+                      << " topic bucket mapping for bucket: "
+                      << bucket->get_name() << " and topic: " << topic.name
+                      << " with ret:" << ret << dendl;
+    return ret;
+  }
+  ldpp_dout(dpp, 20) << "Successfully " << str
+                     << " topic bucket mapping for bucket: "
+                     << bucket->get_name() << " and topic: " << topic.name
+                     << dendl;
+  return ret;
+}
+
+int RadosStore::get_bucket_topic_mapping(const rgw_pubsub_topic& topic,
+                                         std::set<std::string>& bucket_keys,
+                                         optional_yield y,
+                                         const DoutPrefixProvider* dpp) {
+  constexpr auto max_chunk = 1024U;
+  std::string start_after;
+  bool more = true;
+  int rval;
+  while (more) {
+    librados::ObjectReadOperation op;
+    std::set<std::string> curr_keys;
+    op.omap_get_keys2(start_after, max_chunk, &curr_keys, &more, &rval);
+    const auto ret =
+        rgw_rados_operate(dpp, rados->get_notif_pool_ctx(),
+                          get_bucket_topic_mapping_oid(topic), &op, nullptr, y);
+    if (ret == -ENOENT) {
+      // mapping object was not created - nothing to do
+      return 0;
+    }
+    if (ret < 0) {
+      // TODO: do we need to check on rval as well as ret?
+      ldpp_dout(dpp, 1)
+          << "ERROR: failed to read bucket topic mapping object for topic: "
+          << topic.name << ", ret= " << ret << dendl;
+      return ret;
+    }
+    bucket_keys.merge(curr_keys);
+  }
+  return 0;
+}
+
+int RadosStore::delete_bucket_topic_omap(const rgw_pubsub_topic& topic,
+                                         optional_yield y,
+                                         const DoutPrefixProvider* dpp) {
+  librados::ObjectWriteOperation op;
+  op.remove();
+  const int ret =
+      rgw_rados_operate(dpp, rados->get_notif_pool_ctx(),
+                        get_bucket_topic_mapping_oid(topic), &op, y);
+  if (ret < 0 && ret != -ENOENT) {
+    ldpp_dout(dpp, -1)
+        << "ERROR: failed removing bucket topic mapping omap for topic: "
+        << topic.name << ", ret=" << ret << dendl;
+    return ret;
+  }
+  ldpp_dout(dpp, 20)
+      << "Successfully deleted topic bucket mapping omap for topic: "
+      << topic.name << dendl;
+  return ret;
+}
+
 int RadosStore::delete_raw_obj(const DoutPrefixProvider *dpp, const rgw_raw_obj& obj, optional_yield y)
 {
   return rados->delete_raw_obj(dpp, obj, y);

src/rgw/driver/rados/rgw_sal_rados.h
@@ -175,6 +175,23 @@ class RadosStore : public StoreDriver {
                         RGWObjVersionTracker* objv_tracker,
                         optional_yield y,
                         const DoutPrefixProvider* dpp) override;
+    /* Update the bucket-topic mapping in the omap, if |add_mapping|=true then
+     * adding the |bucket_str| to omap, else delete the |bucket_str| from the
+     * omap.  */
+    int update_bucket_topic_mapping(const rgw_pubsub_topic& topic,
+                                    rgw::sal::Bucket* bucket,
+                                    bool add_mapping,
+                                    optional_yield y,
+                                    const DoutPrefixProvider* dpp) override;
+    /** Get the bucket-topic mapping from the omap */
+    int get_bucket_topic_mapping(const rgw_pubsub_topic& topic,
+                                 std::set<std::string>& bucket_keys,
+                                 optional_yield y,
+                                 const DoutPrefixProvider* dpp) override;
+    /** Remove the bucket-topic mapping omap */
+    int delete_bucket_topic_omap(const rgw_pubsub_topic& topic,
+                                 optional_yield y,
+                                 const DoutPrefixProvider* dpp) override;
     virtual RGWLC* get_rgwlc(void) override { return rados->get_lc(); }
     virtual RGWCoroutinesManagerRegistry* get_cr_registry() override { return rados->get_cr_registry(); }
 

src/rgw/rgw_sal.h
@@ -328,6 +328,23 @@ class Driver {
                                 RGWObjVersionTracker* objv_tracker,
                                 optional_yield y,
                                 const DoutPrefixProvider* dpp) = 0;
+    /* Update the bucket-topic mapping in the omap, if |add_mapping|=true then
+     * adding the |bucket_str| to omap, else delete the |bucket_str| from the
+     * omap.  */
+    virtual int update_bucket_topic_mapping(const rgw_pubsub_topic& topic,
+                                            rgw::sal::Bucket* bucket,
+                                            bool add_mapping,
+                                            optional_yield y,
+                                            const DoutPrefixProvider* dpp) = 0;
+    /** Get the bucket-topic mapping from the omap */
+    virtual int get_bucket_topic_mapping(const rgw_pubsub_topic& topic,
+                                         std::set<std::string>& bucket_keys,
+                                         optional_yield y,
+                                         const DoutPrefixProvider* dpp) = 0;
+    /** Remove the bucket-topic mapping omap */
+    virtual int delete_bucket_topic_omap(const rgw_pubsub_topic& topic,
+                                         optional_yield y,
+                                         const DoutPrefixProvider* dpp) = 0;
     /** Get access to the lifecycle management thread */
     virtual RGWLC* get_rgwlc(void) = 0;
     /** Get access to the coroutine registry.  Used to create new coroutine managers */

src/rgw/rgw_sal_filter.h
@@ -218,7 +218,30 @@ public:
                       const DoutPrefixProvider* dpp) override {
     return next->remove_topic_v2(topic_name, tenant, objv_tracker, y, dpp);
   }
-
+  /* Update the bucket-topic mapping in the omap, if |add_mapping|=true then
+   * adding the |bucket_str| to omap, else delete the |bucket_str| from the
+   * omap.  */
+  int update_bucket_topic_mapping(const rgw_pubsub_topic& topic,
+                                  rgw::sal::Bucket* bucket,
+                                  bool add_mapping,
+                                  optional_yield y,
+                                  const DoutPrefixProvider* dpp) override {
+    return next->update_bucket_topic_mapping(topic, bucket, add_mapping, y,
+                                             dpp);
+  }
+  /** Get the bucket-topic mapping from the omap */
+  int get_bucket_topic_mapping(const rgw_pubsub_topic& topic,
+                               std::set<std::string>& bucket_keys,
+                               optional_yield y,
+                               const DoutPrefixProvider* dpp) override {
+    return next->get_bucket_topic_mapping(topic, bucket_keys, y, dpp);
+  }
+  /** Remove the bucket-topic mapping omap */
+  int delete_bucket_topic_omap(const rgw_pubsub_topic& topic,
+                               optional_yield y,
+                               const DoutPrefixProvider* dpp) override {
+    return next->delete_bucket_topic_omap(topic, y, dpp);
+  }
   virtual RGWLC* get_rgwlc(void) override;
   virtual RGWCoroutinesManagerRegistry* get_cr_registry() override;
 

src/rgw/rgw_sal_store.h
@@ -46,14 +46,37 @@ class StoreDriver : public Driver {
                        RGWObjVersionTracker* objv_tracker,
                        optional_yield y,
                        const DoutPrefixProvider* dpp) override {
-      return -ENOENT;
+      return -EOPNOTSUPP;
     }
     int remove_topic_v2(const std::string& topic_name,
                         const std::string& tenant,
                         RGWObjVersionTracker* objv_tracker,
                         optional_yield y,
                         const DoutPrefixProvider* dpp) override {
-      return -ENOENT;
+      return -EOPNOTSUPP;
+    }
+    /* Update the bucket-topic mapping in the omap, if |add_mapping|=true then
+     * adding the |bucket_str| to omap, else delete the |bucket_str| from the
+     * omap.  */
+    int update_bucket_topic_mapping(const rgw_pubsub_topic& topic,
+                                    rgw::sal::Bucket* bucket,
+                                    bool add_mapping,
+                                    optional_yield y,
+                                    const DoutPrefixProvider* dpp) override {
+      return -EOPNOTSUPP;
+    }
+    /** Get the bucket-topic mapping from the omap */
+    int get_bucket_topic_mapping(const rgw_pubsub_topic& topic,
+                                 std::set<std::string>& bucket_keys,
+                                 optional_yield y,
+                                 const DoutPrefixProvider* dpp) override {
+      return -EOPNOTSUPP;
+    }
+    /** Remove the bucket-topic mapping omap */
+    int delete_bucket_topic_omap(const rgw_pubsub_topic& topic,
+                                 optional_yield y,
+                                 const DoutPrefixProvider* dpp) override {
+      return -EOPNOTSUPP;
     }
 };
 

scantist-ossops-m2/ceph-ci

src/rgw/driver/rados/rgw_bucket.cc
@@ -2132,6 +2132,82 @@ int RGWMetadataHandlerPut_Bucket::put_post(const DoutPrefixProvider *dpp)
   return ret;
 }
 
+int update_bucket_topic_mappings(const DoutPrefixProvider* dpp,
+                                 RGWBucketCompleteInfo* orig_bci,
+                                 RGWBucketCompleteInfo* current_bci,
+                                 rgw::sal::Driver* driver) {
+  const auto decode_attrs = [](const rgw::sal::Attrs& attrs,
+                               rgw_pubsub_bucket_topics& bucket_topics) -> int {
+    auto iter = attrs.find(RGW_ATTR_BUCKET_NOTIFICATION);
+    if (iter == attrs.end()) {
+      return 0;
+    }
+    try {
+      const auto& bl = iter->second;
+      auto biter = bl.cbegin();
+      bucket_topics.decode(biter);
+    } catch (buffer::error& err) {
+      return -EIO;
+    }
+    return 0;
+  };
+  rgw_pubsub_bucket_topics old_bucket_topics;
+  if (orig_bci) {
+    auto ret = decode_attrs(orig_bci->attrs, old_bucket_topics);
+    if (ret < 0) {
+      ldpp_dout(dpp, 1)
+          << "ERROR: failed to decode OLD bucket topics for bucket: "
+          << orig_bci->info.bucket.name << dendl;
+      return ret;
+    }
+  }
+  rgw_pubsub_bucket_topics current_bucket_topics;
+  auto ret = decode_attrs(current_bci->attrs, current_bucket_topics);
+  if (ret < 0) {
+    ldpp_dout(dpp, 1)
+        << "ERROR: failed to decode current bucket topics for bucket: "
+        << current_bci->info.bucket.name << dendl;
+    return ret;
+  }
+  // fetch the list of subscribed topics stored inside old_bucket attrs.
+  std::unordered_map<std::string, rgw_pubsub_topic> old_topics;
+  for (const auto& [_, topic_filter] : old_bucket_topics.topics) {
+    old_topics[topic_filter.topic.name] = topic_filter.topic;
+  }
+  // fetch the list of subscribed topics stored inside current_bucket attrs.
+  std::unordered_map<std::string, rgw_pubsub_topic> current_topics;
+  for (const auto& [_, topic_filter] : current_bucket_topics.topics) {
+    current_topics[topic_filter.topic.name] = topic_filter.topic;
+  }
+  auto bucket = driver->get_bucket(current_bci->info);
+  // traverse thru old topics and check if they are not in current, then delete
+  // the mapping, if present in both current and old then delete from current
+  // set as we do not need to update those mapping.
+  for (const auto& [topic_name, topic] : old_topics) {
+    auto it = current_topics.find(topic_name);
+    if (it == current_topics.end()) {
+      const auto op_ret = driver->update_bucket_topic_mapping(
+          topic, bucket.get(), /*add_mapping=*/false, null_yield, dpp);
+      if (op_ret < 0) {
+        ret = op_ret;
+      }
+    } else {
+      // already that attr is present, so do not update the mapping.
+      current_topics.erase(it);
+    }
+  }
+  // traverse thru current topics and check if they are any present, then add
+  // the mapping.
+  for (const auto& [topic_name, topic] : current_topics) {
+    const auto op_ret = driver->update_bucket_topic_mapping(
+        topic, bucket.get(), /*add_mapping=*/true, null_yield, dpp);
+    if (op_ret < 0) {
+      ret = op_ret;
+    }
+  }
+  return ret;
+}
+
 static void get_md5_digest(const RGWBucketEntryPoint *be, string& md5_digest) {
 
    char md5[CEPH_CRYPTO_MD5_DIGESTSIZE * 2 + 1];
@@ -2648,6 +2724,21 @@ int RGWMetadataHandlerPut_BucketInstance::put_post(const DoutPrefixProvider *dpp
     }
   } /* update lc */
 
+  /* update bucket topic mapping */
+  {
+    auto* orig_obj = static_cast<RGWBucketInstanceMetadataObject*>(old_obj);
+    auto* orig_bci = (orig_obj ? &orig_obj->get_bci() : nullptr);
+    ret = update_bucket_topic_mappings(dpp, orig_bci, &bci, bihandler->driver);
+    if (ret < 0) {
+      ldpp_dout(dpp, 0) << __func__
+                        << " failed to apply bucket topic mapping for "
+                        << bci.info.bucket.name << dendl;
+      return ret;
+    }
+    ldpp_dout(dpp, 20) << __func__
+                       << " successfully applied bucket topic mapping for "
+                       << bci.info.bucket.name << dendl;
+  }
   return STATUS_APPLIED;
 }
 

src/rgw/driver/rados/rgw_sal_rados.cc
@@ -1170,6 +1170,90 @@ int RadosStore::remove_topic_v2(const std::string& topic_name,
                                            params, objv_tracker, y, dpp);
 }
 
+int RadosStore::update_bucket_topic_mapping(const rgw_pubsub_topic& topic,
+                                            rgw::sal::Bucket* bucket,
+                                            bool add_mapping,
+                                            optional_yield y,
+                                            const DoutPrefixProvider* dpp) {
+  const auto& bucket_str =
+      rgw_make_bucket_entry_name(bucket->get_tenant(), bucket->get_name());
+  bufferlist empty_bl;
+  librados::ObjectWriteOperation op;
+  int ret = 0;
+  if (add_mapping) {
+    std::map<std::string, bufferlist> mapping{{bucket_str, empty_bl}};
+    op.omap_set(mapping);
+  } else {
+    std::set<std::string> to_rm{{bucket_str}};
+    op.omap_rm_keys(to_rm);
+  }
+  ret = rgw_rados_operate(dpp, rados->get_notif_pool_ctx(),
+                          get_bucket_topic_mapping_oid(topic), &op, y);
+  if (ret < 0) {
+    ldpp_dout(dpp, 1) << "ERROR: failed to " << (add_mapping ? "add" : "remove")
+                      << " topic bucket mapping for bucket: "
+                      << bucket->get_name() << " and topic: " << topic.name
+                      << " with ret:" << ret << dendl;
+    return ret;
+  }
+  ldpp_dout(dpp, 20) << "Successfully " << (add_mapping ? "added" : "removed")
+                     << " topic bucket mapping for bucket: "
+                     << bucket->get_name() << " and topic: " << topic.name
+                     << dendl;
+  return ret;
+}
+
+int RadosStore::get_bucket_topic_mapping(const rgw_pubsub_topic& topic,
+                                         std::set<std::string>& bucket_keys,
+                                         optional_yield y,
+                                         const DoutPrefixProvider* dpp) {
+  constexpr auto max_chunk = 1024U;
+  std::string start_after;
+  bool more = true;
+  int rval;
+  while (more) {
+    librados::ObjectReadOperation op;
+    std::set<std::string> curr_keys;
+    op.omap_get_keys2(start_after, max_chunk, &curr_keys, &more, &rval);
+    const auto ret =
+        rgw_rados_operate(dpp, rados->get_notif_pool_ctx(),
+                          get_bucket_topic_mapping_oid(topic), &op, nullptr, y);
+    if (ret == -ENOENT) {
+      // mapping object was not created - nothing to do
+      return 0;
+    }
+    if (ret < 0) {
+      // TODO: do we need to check on rval as well as ret?
+      ldpp_dout(dpp, 1)
+          << "ERROR: failed to read bucket topic mapping object for topic: "
+          << topic.name << ", ret= " << ret << dendl;
+      return ret;
+    }
+    bucket_keys.merge(curr_keys);
+  }
+  return 0;
+}
+
+int RadosStore::delete_bucket_topic_mapping(const rgw_pubsub_topic& topic,
+                                            optional_yield y,
+                                            const DoutPrefixProvider* dpp) {
+  librados::ObjectWriteOperation op;
+  op.remove();
+  const int ret =
+      rgw_rados_operate(dpp, rados->get_notif_pool_ctx(),
+                        get_bucket_topic_mapping_oid(topic), &op, y);
+  if (ret < 0 && ret != -ENOENT) {
+    ldpp_dout(dpp, 1)
+        << "ERROR: failed removing bucket topic mapping omap for topic: "
+        << topic.name << ", ret=" << ret << dendl;
+    return ret;
+  }
+  ldpp_dout(dpp, 20)
+      << "Successfully deleted topic bucket mapping omap for topic: "
+      << topic.name << dendl;
+  return 0;
+}
+
 int RadosStore::delete_raw_obj(const DoutPrefixProvider *dpp, const rgw_raw_obj& obj, optional_yield y)
 {
   return rados->delete_raw_obj(dpp, obj, y);

src/rgw/driver/rados/rgw_sal_rados.h
@@ -175,6 +175,23 @@ class RadosStore : public StoreDriver {
                         RGWObjVersionTracker* objv_tracker,
                         optional_yield y,
                         const DoutPrefixProvider* dpp) override;
+    /* Update the bucket-topic mapping in the omap, if |add_mapping|=true then
+     * adding the |bucket_str| to omap, else delete the |bucket_str| from the
+     * omap.  */
+    int update_bucket_topic_mapping(const rgw_pubsub_topic& topic,
+                                    rgw::sal::Bucket* bucket,
+                                    bool add_mapping,
+                                    optional_yield y,
+                                    const DoutPrefixProvider* dpp) override;
+    /** Get the bucket-topic mapping from the omap */
+    int get_bucket_topic_mapping(const rgw_pubsub_topic& topic,
+                                 std::set<std::string>& bucket_keys,
+                                 optional_yield y,
+                                 const DoutPrefixProvider* dpp) override;
+    /** Remove the bucket-topic mapping omap */
+    int delete_bucket_topic_mapping(const rgw_pubsub_topic& topic,
+                                    optional_yield y,
+                                    const DoutPrefixProvider* dpp) override;
     virtual RGWLC* get_rgwlc(void) override { return rados->get_lc(); }
     virtual RGWCoroutinesManagerRegistry* get_cr_registry() override { return rados->get_cr_registry(); }
 

src/rgw/rgw_sal.h
@@ -328,6 +328,23 @@ class Driver {
                                 RGWObjVersionTracker* objv_tracker,
                                 optional_yield y,
                                 const DoutPrefixProvider* dpp) = 0;
+    /* Update the bucket-topic mapping in the omap, if |add_mapping|=true then
+     * adding the |bucket_str| to omap, else delete the |bucket_str| from the
+     * omap.  */
+    virtual int update_bucket_topic_mapping(const rgw_pubsub_topic& topic,
+                                            rgw::sal::Bucket* bucket,
+                                            bool add_mapping,
+                                            optional_yield y,
+                                            const DoutPrefixProvider* dpp) = 0;
+    /** Get the bucket-topic mapping from the omap */
+    virtual int get_bucket_topic_mapping(const rgw_pubsub_topic& topic,
+                                         std::set<std::string>& bucket_keys,
+                                         optional_yield y,
+                                         const DoutPrefixProvider* dpp) = 0;
+    /** Remove the bucket-topic mapping omap */
+    virtual int delete_bucket_topic_mapping(const rgw_pubsub_topic& topic,
+                                            optional_yield y,
+                                            const DoutPrefixProvider* dpp) = 0;
     /** Get access to the lifecycle management thread */
     virtual RGWLC* get_rgwlc(void) = 0;
     /** Get access to the coroutine registry.  Used to create new coroutine managers */

src/rgw/rgw_sal_filter.h
@@ -218,7 +218,30 @@ public:
                       const DoutPrefixProvider* dpp) override {
     return next->remove_topic_v2(topic_name, tenant, objv_tracker, y, dpp);
   }
-
+  /* Update the bucket-topic mapping in the omap, if |add_mapping|=true then
+   * adding the |bucket_str| to omap, else delete the |bucket_str| from the
+   * omap.  */
+  int update_bucket_topic_mapping(const rgw_pubsub_topic& topic,
+                                  rgw::sal::Bucket* bucket,
+                                  bool add_mapping,
+                                  optional_yield y,
+                                  const DoutPrefixProvider* dpp) override {
+    return next->update_bucket_topic_mapping(topic, bucket, add_mapping, y,
+                                             dpp);
+  }
+  /** Get the bucket-topic mapping from the omap */
+  int get_bucket_topic_mapping(const rgw_pubsub_topic& topic,
+                               std::set<std::string>& bucket_keys,
+                               optional_yield y,
+                               const DoutPrefixProvider* dpp) override {
+    return next->get_bucket_topic_mapping(topic, bucket_keys, y, dpp);
+  }
+  /** Remove the bucket-topic mapping omap */
+  int delete_bucket_topic_mapping(const rgw_pubsub_topic& topic,
+                                  optional_yield y,
+                                  const DoutPrefixProvider* dpp) override {
+    return next->delete_bucket_topic_mapping(topic, y, dpp);
+  }
   virtual RGWLC* get_rgwlc(void) override;
   virtual RGWCoroutinesManagerRegistry* get_cr_registry() override;
 

src/rgw/rgw_sal_store.h
@@ -46,14 +46,37 @@ class StoreDriver : public Driver {
                        RGWObjVersionTracker* objv_tracker,
                        optional_yield y,
                        const DoutPrefixProvider* dpp) override {
-      return -ENOENT;
+      return -EOPNOTSUPP;
     }
     int remove_topic_v2(const std::string& topic_name,
                         const std::string& tenant,
                         RGWObjVersionTracker* objv_tracker,
                         optional_yield y,
                         const DoutPrefixProvider* dpp) override {
-      return -ENOENT;
+      return -EOPNOTSUPP;
+    }
+    /* Update the bucket-topic mapping in the omap, if |add_mapping|=true then
+     * adding the |bucket_str| to omap, else delete the |bucket_str| from the
+     * omap.  */
+    int update_bucket_topic_mapping(const rgw_pubsub_topic& topic,
+                                    rgw::sal::Bucket* bucket,
+                                    bool add_mapping,
+                                    optional_yield y,
+                                    const DoutPrefixProvider* dpp) override {
+      return -EOPNOTSUPP;
+    }
+    /** Get the bucket-topic mapping from the omap */
+    int get_bucket_topic_mapping(const rgw_pubsub_topic& topic,
+                                 std::set<std::string>& bucket_keys,
+                                 optional_yield y,
+                                 const DoutPrefixProvider* dpp) override {
+      return -EOPNOTSUPP;
+    }
+    /** Remove the bucket-topic mapping omap */
+    int delete_bucket_topic_mapping(const rgw_pubsub_topic& topic,
+                                    optional_yield y,
+                                    const DoutPrefixProvider* dpp) override {
+      return -EOPNOTSUPP;
     }
 };
 