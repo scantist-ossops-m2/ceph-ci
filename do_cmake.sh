f82249f6f8e5c2290a8ed11f83ad9c01270191b7
Adam Kupczyk <akupczyk@ibm.com>
2024-01-09 12:24:28 +0000

    build: enable intel quickassis
    Just for tests for now.
    
    Signed-off-by: Adam Kupczyk <akupczyk@ibm.com>

--- do_cmake.sh
+++ do_cmake.sh
@@ -78,6 +78,7 @@ for i in $(seq 20 -1 11); do
 done
 ARGS+=" -DCMAKE_CXX_COMPILER=$cxx_compiler"
 ARGS+=" -DCMAKE_C_COMPILER=$c_compiler"
+ARGS+=" -DWITH_QAT=ON -DWITH_QATZIP=ON"
 
 mkdir $BUILD_DIR
 cd $BUILD_DIR
