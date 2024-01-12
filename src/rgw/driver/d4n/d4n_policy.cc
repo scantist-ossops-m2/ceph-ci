4d525417b006ba7aa9f7770a6afeec90b15a109b
Samarah <samarah.uriarte@ibm.com>
2024-01-12 19:01:05 +0000

    rgw/d4n: Perform minor cleanup and add error checking for `localWeights` in policy
    Signed-off-by: Samarah <samarah.uriarte@ibm.com>

--- src/rgw/driver/d4n/d4n_policy.cc
+++ src/rgw/driver/d4n/d4n_policy.cc
@@ -355,7 +355,12 @@ void LFUDAPolicy::update(const DoutPrefixProvider* dpp, std::string& key, uint64
   if (cacheDriver->set_attr(dpp, key, "user.rgw.localWeight", std::to_string(localWeight), y) < 0) 
     ldpp_dout(dpp, 10) << "LFUDAPolicy::" << __func__ << "(): CacheDriver set_attr method failed." << dendl;
 
-  auto localWeights = get_local_weight_sum(y);
+  int localWeights = get_local_weight_sum(y);
+  if (localWeights < 0) {
+    ldpp_dout(dpp, 10) << "LFUDAPolicy::" << __func__ << "(): Failed to retrieve sum of local weights for the cache backend." << dendl;
+    return;
+  }
+
   localWeights += ((localWeight < 0) ? 0 : localWeight);
   if (set_local_weight_sum(localWeights, y) < 0)
     ldpp_dout(dpp, 10) << "LFUDAPolicy::" << __func__ << "(): Failed to update sum of local weights for the cache backend." << dendl;
@@ -368,10 +373,17 @@ bool LFUDAPolicy::erase(const DoutPrefixProvider* dpp, const std::string& key, o
     return false;
   }
 
-  auto localWeights = get_local_weight_sum(y);
+  int localWeights = get_local_weight_sum(y);
+  if (localWeights < 0) {
+    ldpp_dout(dpp, 10) << "LFUDAPolicy::" << __func__ << "(): Failed to retrieve sum of local weights for the cache backend." << dendl;
+    return false;
+  }
+
   localWeights -= ((p->second->localWeight < 0) ? 0 : p->second->localWeight);
-  if (set_local_weight_sum(localWeights, y) < 0)
+  if (set_local_weight_sum(localWeights, y) < 0) {
     ldpp_dout(dpp, 10) << "LFUDAPolicy::" << __func__ << "(): Failed to update sum of local weights for the cache backend." << dendl;
+    return false;
+  }
 
   entries_map.erase(p);
   entries_heap.erase(p->second->handle);
