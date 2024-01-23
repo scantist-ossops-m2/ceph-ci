8c9a632a12d7cc109eabd9cb3382fb3e6d66c61c
Yingxin Cheng <yingxin.cheng@intel.com>
2024-01-23 10:37:46 +0800

    crimson/osd/osd_operations/client_request: cleanup, simplify recover_missings
    Signed-off-by: Yingxin Cheng <yingxin.cheng@intel.com>

--- src/crimson/osd/osd_operations/client_request_common.h
+++ src/crimson/osd/osd_operations/client_request_common.h
@@ -12,7 +12,7 @@ namespace crimson::osd {
 struct CommonClientRequest {
 
   static InterruptibleOperation::template interruptible_future<>
-  do_recover_missing(Ref<PG>& pg, const hobject_t& soid);
+  do_recover_missing(Ref<PG> pg, const hobject_t& soid);
 
   static bool should_abort_request(
     const crimson::Operation& op, std::exception_ptr eptr);
