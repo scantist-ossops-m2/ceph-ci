1be89f0ac21119f089cc475f043af819d29f0b08
Xiubo Li <xiubli@redhat.com>
2024-03-27 18:08:53 +0800

    b

--- qa/workunits/fs/fscrypt_protect.sh
+++ qa/workunits/fs/fscrypt_protect.sh
@@ -6,15 +6,18 @@ client_type=$1
 
 case ${client_type} in
 	"kclient")
-		mkdir fscrypt_dir
-		echo "hello" > fscrypt_dir/fscrypt_test_file
-		touch fscrypt_kclient_ready
-		while [ ! -f fscrypt_fuse_ready ] ;
-			pwd;
-			ls -R;
-			do sleep 1;
+		echo "hello" > fscrypt_test_file
+		THEN=`stat -c %z fscrypt_test_file`
+		NOW=`stat -c %z fscrypt_test_file`
+		echo $THEN
+		echo $NOW
+		while [ "$NOW" == "$THEN" ]
+		do
+			sleep 5
+			NOW=`stat -c %z fscrypt_test_file`
+			echo $NOW
 		done
-		if grep -q "hello" fscrypt_dir/fscrypt_test_file; then
+		if grep -q "hello" fscrypt_test_file; then
 			echo "Fscrypt-protect test successfully!"
 		else
 			echo "Fscrypt-protect test failed!"
@@ -22,17 +25,26 @@ case ${client_type} in
 		fi
 		;;
 	"fuse")
-		while [ ! -f fscrypt_kclient_ready ] ;
-			pwd;
-			ls -R;
-			do sleep 1;
+		num=$(ls -l | wc -l)
+		while [ $num -ne 2 ]
+		do
+			sleep 5
+			num=$(ls -l | wc -l)
 		done
 
-		set +e
-		echo > fscrypt_dir/*
-		set -e
+		# just wait for a while to make sure the $THEN is correctly
+		# updated in kernel client
+		sleep 10
 
-		touch fscrypt_fuse_ready
+		set +e
+		echo > ./*
+		set -e
+		touch *
+
+		# wait the kclient test to finish before removing the shared
+		# files when unmounting
+		sleep 60
+
 		;;
 	*)
 		echo "Unknown client type $1"
